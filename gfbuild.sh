61dfe77c81523e7b8727e59a142584272d7ebee7
Romain Grecourt <romain.grecourt@oracle.com>
2018-09-26 13:51:32 -0700

    more troubleshooting

--- gfbuild.sh
+++ gfbuild.sh
@@ -97,6 +97,13 @@ if [ ! -z "${JENKINS_HOME}" ] ; then
   echo "df" ; df -h
   echo "mount" ; mount
   echo "id" ; id
+  echo "chmod ugo+w -R /usr/share/maven" ; chmod ugo+w -R /usr/share/maven
+  echo "touch /usr/ok1" ; touch /usr/ok1
+  echo "touch /usr/share/ok2" ; touch /usr/share/ok2
+  echo "touch /usr/share/maven/ok3" ; touch /usr/share/maven/ok3
+  echo "touch /usr/share/maven/lib/ok4" ; touch /usr/share/maven/lib/ok4
+
+
 
   # install takari extensions for
   # process/thread safe access to the local repository

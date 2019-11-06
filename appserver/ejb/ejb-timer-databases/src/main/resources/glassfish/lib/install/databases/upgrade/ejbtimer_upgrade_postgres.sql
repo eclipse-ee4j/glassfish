b74faa693a239270016689e758ac1db02daf8101
endtak <endo.takafumi@fujitsu.com>
2019-11-06 11:59:47 +0900

    Fix for copyright.
    Signed-off-by: endtak <endo.takafumi@fujitsu.com>

--- appserver/ejb/ejb-timer-databases/src/main/resources/glassfish/lib/install/databases/upgrade/ejbtimer_upgrade_postgres.sql
+++ appserver/ejb/ejb-timer-databases/src/main/resources/glassfish/lib/install/databases/upgrade/ejbtimer_upgrade_postgres.sql
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright:qw:wq (c) 2019 Fujitsu Limited and/or its affiliates. All rights
+ * reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at

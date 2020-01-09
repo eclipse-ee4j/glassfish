19c77c6b27346f41cb9b63f0c50cf8cb07ac1bba
Piotrek Zygielo <piotr@zygielo.pl>
2020-01-09 12:21:59 +0100

    If not set explicitly assume async is not supported and avoid NPEx
    Fixes #22406
    
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- appserver/web/web-glue/src/main/java/com/sun/enterprise/web/deploy/FilterDefDecorator.java
+++ appserver/web/web-glue/src/main/java/com/sun/enterprise/web/deploy/FilterDefDecorator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -90,7 +90,8 @@ public class FilterDefDecorator extends FilterDef {
     }
 
     public boolean isAsyncSupported() {
-        return decoree.isAsyncSupported();
+        Boolean decoreeAsyncFlag = decoree.isAsyncSupported();
+        return (decoreeAsyncFlag == null ? false : decoreeAsyncFlag);
     }   
 
 }

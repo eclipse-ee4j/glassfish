73a236e95b720a46bd11e43e5e2a264974423117
arjantijms <arjan.tijms@gmail.com>
2020-03-18 17:13:49 +0100

    Added path redundancy in scripts and cleanings
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

--- appserver/tests/gftest.sh
+++ appserver/tests/gftest.sh
@@ -15,9 +15,14 @@
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 #
 
-run_test(){
+run_test() {
+    echo "Cleaning test state"
+    cd `dirname ${0}`
+    git clean -xf || true
+    
 	local testid=${1}
 	local found=false
+	
 	for runtest in `find . -name run_test\.sh`; do
 		for id in `${runtest} list_test_ids`; do
 			if [[ "${id}" = "${testid}" ]]; then
@@ -30,6 +35,7 @@ run_test(){
 			break
 		fi
 	done
+	
 	if [[ "${found}" = false ]]; then
 		echo Invalid Test Id.
 		exit 1
@@ -38,13 +44,15 @@ run_test(){
 
 if [ ! -z "${JENKINS_HOME}" ] ; then
 
-  # inject internal environment
+  # Inject internal environment
   readonly GF_INTERNAL_ENV_SH=$(mktemp -t XXXgf-internal-env)
+  
   if [ ! -z "${GF_INTERNAL_ENV}" ] ; then
     echo "${GF_INTERNAL_ENV}" | base64 -d > ${GF_INTERNAL_ENV_SH}
     . ${GF_INTERNAL_ENV_SH}
     export MAVEN_OPTS="${MAVEN_OPTS} ${ANT_OPTS}"
   fi
+  
   export WSIMPORT_OPTS="${ANT_OPTS}"
 
   # setup the local repository

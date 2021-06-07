2f4c277ae909ecdee8d0c1efbbd18229e1fe935c
David Matějček <dmatej@seznam.cz>
2021-06-07 09:07:32 +0200

    Issue #23485 Added profile and script for changing the version id
    - previous commits are prerequisity of this one
    - the profile collects all modules to one reactor, except appserver/tests
    - appserver/tests are the reason for the script, which does the change
      as a text processor, so it ignores all broken dependencies.
    - alternative ways to change the version id are still possible, but this is
      easier. Just run ./updateVersion.sh 7.0.0-MY-SNAPSHOT etc.
    - both script and the profile can be deleted after we fix all modules in
      the repository

---
+++ updateVersion.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+set -e
+
+version=${1}
+if [ "${version}" == "" ]; then
+    echo "New version number expected!"
+    exit 1;
+fi
+
+oldVersion="$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" ./pom.xml)"
+implicitVersionParams="-DgenerateBackupPoms=false -DprocessAllModules=true -DoldVersion=${oldVersion}"
+
+# change version of the aggregator and keep consistency
+mvn versions:set ${implicitVersionParams} -DnewVersion=${version} -Pembedded,set-version-id;
+
+# these folders are broken now, but we keep updating version ids at least
+find "./appserver/tests" -type f -name "pom.xml" -print0 | while IFS= read -r -d '' file; do
+    echo "Processing file: ${file}";
+    sedexpression="s/${oldVersion}/${version}/g";
+    cat "${file}" | sed -e "${sedexpression}" > "${file}.temporary";
+    fileMode=$(stat -c %a "${file}");
+    mv "${file}.temporary" "${file}";
+    chmod $fileMode "${file}";
+done
+

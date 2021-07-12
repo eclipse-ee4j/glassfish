23e76d4ca2382f1309a873f21fb1066a24362b3c
Piotrek Zygielo <piotr@zygielo.pl>
2021-07-13 01:03:45 +0200

    Adjust pattern for 23521 (no count in repo w/o tags)

--- appserver/distributions/glassfish-common/src/it/build-id/postbuild.groovy
+++ appserver/distributions/glassfish-common/src/it/build-id/postbuild.groovy
@@ -8,6 +8,6 @@ buildId = properties['build_id']
 assert !buildId.matches('.*\\$\\{.*\\}.*') :
   "Build ID (${buildId}) contains unresolved property"
 
-assert buildId.matches('.+-b\\p{Digit}+-g\\p{XDigit}+ [\\p{Digit}-]+T[\\p{Digit}:]+\\+\\p{Digit}+') :
+assert buildId.matches('.+-b\\p{Digit}*-g\\p{XDigit}+ [\\p{Digit}-]+T[\\p{Digit}:]+\\+\\p{Digit}+') :
   "Build ID (${buildId}) does not match expected pattern"
 

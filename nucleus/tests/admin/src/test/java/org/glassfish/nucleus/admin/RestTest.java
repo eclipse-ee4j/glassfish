4ce57251bd12a83ddc7cf61fe625565a67a81f7c
David Matějček <dmatej@seznam.cz>
2021-07-30 23:40:30 +0200

    Issue #23507 QuickLook tests removed
    - just the RestTest was moved under the admin module because it tests admin's
      endpoint
    - these tests will be rewritten soon in much better way
    - currently they were not executed

--- nucleus/tests/admin/src/test/java/org/glassfish/nucleus/admin/RestTest.java
+++ nucleus/tests/admin/src/test/java/org/glassfish/nucleus/admin/RestTest.java
@@ -14,7 +14,7 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-package org.glassfish.nucleus.quicklook;
+package org.glassfish.nucleus.admin;
 
 import java.io.DataOutputStream;
 import java.io.IOException;

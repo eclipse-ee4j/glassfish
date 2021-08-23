11b4df6e5027a1d1dd917092e0854c10afa7665e
David Matějček <dmatej@seznam.cz>
2021-08-23 20:28:45 +0200

    Issue #23507 Added missing license headers

--- nucleus/hk2/hk2-config/src/test/java/org/jvnet/hk2/config/test/example/GenericContainer.java
+++ nucleus/hk2/hk2-config/src/test/java/org/jvnet/hk2/config/test/example/GenericContainer.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at

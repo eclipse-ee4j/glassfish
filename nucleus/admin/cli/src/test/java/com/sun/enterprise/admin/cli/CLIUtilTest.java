88c599fe662ebba0f013c034d9e0f2301112f31e
David Matějček <dmatej@seznam.cz>
2021-08-29 18:16:58 +0200

    Issue #23507 admin-cli now uses junit5 instead of junit4 + deleted zombies
    - two test classes were commented out and not usable

--- nucleus/admin/cli/src/test/java/com/sun/enterprise/admin/cli/CLIUtilTest.java
+++ nucleus/admin/cli/src/test/java/com/sun/enterprise/admin/cli/CLIUtilTest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2008, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -16,53 +17,35 @@
 
 package com.sun.enterprise.admin.cli;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-import org.junit.Before;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.util.Map;
-import org.glassfish.api.admin.CommandException;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
 /**
  * junit test to test CLIUtil class
  */
 public class CLIUtilTest {
+
     @Test
-    public void getUploadFileTest() {
-        BufferedWriter out = null;
-        String fileName = null;
-        try {
-            final File f = File.createTempFile("TestPasswordFile", ".tmp");
-            fileName = f.toString();
-            f.deleteOnExit();
-            out = new BufferedWriter(new FileWriter(f));
+    public void getUploadFileTest() throws Exception {
+        final File f = File.createTempFile("TestPasswordFile", ".tmp");
+        String fileName = f.toString();
+        f.deleteOnExit();
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(f))) {
             out.write("AS_ADMIN_PASSWORD=adminadmin\n");
             out.write("AS_ADMIN_MASTERPASSWORD=changeit\n");
         }
-        catch (IOException ioe) {
-        }
-        finally {
-            try {
-                if (out != null)
-                    out.close();
-            } catch(final Exception ignore){}
-        }
-        try {
-            Map<String, String> po = CLIUtil.readPasswordFileOptions(fileName, false);
-            assertEquals("admin password", "adminadmin", po.get("password"));
-            assertEquals("master password", "changeit", po.get("masterpassword"));
-            assertEquals("null", null, po.get("foobar"));
-        }
-        catch (CommandException ce) {
-            ce.printStackTrace();
-        }
-    }
 
-    @Before
-    public void setup() {
+        Map<String, String> po = CLIUtil.readPasswordFileOptions(fileName, false);
+        assertEquals("adminadmin", po.get("password"), "admin password");
+        assertEquals("changeit", po.get("masterpassword"), "master password");
+        assertNull(po.get("foobar"), "null");
     }
 }

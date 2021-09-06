d520f0f187e928fc4d606b50293bcff2dabaa1ae
David Matějček <dmatej@seznam.cz>
2021-09-06 23:25:31 +0200

    Issue #23507 gf-client-module now uses junit5 instead of junit4
    - DefaultGUICallbackHandlerTest can be executed in Eclipse, depends on AWT UI
    - several tests enabled
    - AppClientContainerTest deleted, depended on unknown classes and was commented out

--- appserver/appclient/client/acc/src/test/java/org/glassfish/appclient/client/acc/AppclientCommandArgumentsTest.java
+++ appserver/appclient/client/acc/src/test/java/org/glassfish/appclient/client/acc/AppclientCommandArgumentsTest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -16,23 +17,27 @@
 
 package org.glassfish.appclient.client.acc;
 
-import java.io.IOException;
-import java.util.Properties;
-import java.io.File;
 import com.sun.enterprise.transaction.JavaEETransactionManagerSimplified;
 import com.sun.logging.LogDomains;
+
+import java.io.File;
 import java.io.FileWriter;
-import java.util.logging.Logger;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.MissingResourceException;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- *
  * @author tjquinn
  */
 public class AppclientCommandArgumentsTest {
@@ -42,136 +47,79 @@ public class AppclientCommandArgumentsTest {
     private static final String EXPECTED_TARGETSERVER_VALUE = "A:1234,B:5678";
     private static final String EXPECTED_PASSWORD_IN_PASSWORD_FILE = "mySecretPassword";
 
-    public AppclientCommandArgumentsTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
     @Test
     public void testA() throws Exception, UserError {
-        AppclientCommandArguments info = AppclientCommandArguments.newInstance(
-                Arrays.asList("-textauth", "-user", USER_VALUE));
+        AppclientCommandArguments info = AppclientCommandArguments
+            .newInstance(Arrays.asList("-textauth", "-user", USER_VALUE));
 
-        assertEquals("text auth not set", true, info.isTextauth());
-        assertEquals("noappinvoke incorrectly set", false, info.isNoappinvoke());
-        assertEquals("user incorrectly set", USER_VALUE, info.getUser());
-        /*
-         * Make sure an unspecified argument is repoted as null.
-         */
-        assertEquals("mainclass wrong", null, info.getMainclass());
-
+        assertTrue(info.isTextauth(), "text auth not set");
+        assertFalse(info.isNoappinvoke(), "noappinvoke incorrectly set");
+        assertEquals(USER_VALUE, info.getUser(), "user incorrectly set");
+        assertNull(info.getMainclass(), "mainclass wrong");
     }
 
     @Test
     public void testB() throws Exception, UserError {
-        AppclientCommandArguments info = AppclientCommandArguments.newInstance(
-                Arrays.asList("-mainclass", PASSWORDFILE_NAME, "-noappinvoke"));
+        AppclientCommandArguments info = AppclientCommandArguments
+            .newInstance(Arrays.asList("-mainclass", PASSWORDFILE_NAME, "-noappinvoke"));
 
-
-        assertEquals("wrong main class", PASSWORDFILE_NAME, info.getMainclass());
-        assertEquals("noappinvoke not set", true, info.isNoappinvoke());
-        assertEquals("user should have been null", null, info.getUser());
-        assertEquals("textauth found but should be absent", false, info.isTextauth());
+        assertEquals(PASSWORDFILE_NAME, info.getMainclass(), "wrong main class");
+        assertEquals(true, info.isNoappinvoke(), "noappinvoke not set");
+        assertEquals(null, info.getUser(), "user should have been null");
+        assertEquals(false, info.isTextauth(), "textauth found but should be absent");
     }
 
-    @Ignore
     @Test
-    public void invalidArgumentTest() throws Exception, UserError {
-        try {
-            AppclientCommandArguments.newInstance(
-                Arrays.asList("-badarg"));
-            fail("did not throw expected IllegalArgumentException due to an invalid arg");
-        } catch (IllegalArgumentException e) {
-            // no-op so test passes
-        } catch (Exception e) {
-            fail("expected IllegalArgumentException but got " + e.getClass().getName());
-        }
+    @Disabled("Doesn't throw anything.")
+    public void invalidArgumentTest() throws Exception {
+        assertThrows(IllegalArgumentException.class,
+            () -> AppclientCommandArguments.newInstance(Arrays.asList("-badarg")));
     }
 
     @Test
     public void disallowMainclassAndName() throws Exception {
-        try {
-            AppclientCommandArguments.newInstance(
-                    Arrays.asList("-mainclass","x.y.Main","-name","some-display-name"));
-            fail("did not detect incorrect spec of mainclass and name");
-        } catch (UserError e) {
-            // suppress exception for a successful test
-        }
+        assertThrows(UserError.class, () -> AppclientCommandArguments
+            .newInstance(Arrays.asList("-mainclass", "x.y.Main", "-name", "some-display-name")));
     }
 
     @Test
     public void allowMultiValuedTargetServer() throws Exception, UserError {
         final AppclientCommandArguments cmdArgs = AppclientCommandArguments.newInstance(
                 Arrays.asList("-targetserver","\"" + EXPECTED_TARGETSERVER_VALUE + "\""));
-        assertEquals("did not process targetserver list correctly",
-                EXPECTED_TARGETSERVER_VALUE,
-                cmdArgs.getTargetServer());
+        assertEquals(EXPECTED_TARGETSERVER_VALUE, cmdArgs.getTargetServer(),
+            "did not process targetserver list correctly");
     }
 
     @Test
     public void useTransactionLogString() {
-        final Logger logger = LogDomains.getLogger(JavaEETransactionManagerSimplified.class,
-                LogDomains.JTA_LOGGER);
+        final Logger logger = LogDomains.getLogger(JavaEETransactionManagerSimplified.class, LogDomains.JTA_LOGGER);
         final String target = "enterprise_used_delegate_name";
-        try {
-            final String result = logger.getResourceBundle().getString(target);
-            assertTrue("message key look-up failed", (result != null &&
-                    ! target.equals(result)));
-        } catch (MissingResourceException ex) {
-            fail("could not find message key");
-        }
+        final String result = logger.getResourceBundle().getString(target);
+        assertEquals("DTX5019: Transaction Manager is ready. Using [{0}] as the delegate", result,
+            "message key look-up failed");
     }
 
     @Test
-    public void checkPasswordInFile() {
+    public void checkPasswordInFile() throws Exception, UserError {
         final Properties props = new Properties();
-        props.setProperty(AppclientCommandArguments.PASSWORD_FILE_PASSWORD_KEYWORD,
-                EXPECTED_PASSWORD_IN_PASSWORD_FILE);
-        try {
-            final AppclientCommandArguments cmdArgs = prepareWithPWFile(props);
-            final char[] pwInObject = cmdArgs.getPassword();
-            assertTrue("Password " + EXPECTED_PASSWORD_IN_PASSWORD_FILE +
-                    " in password file does not match password " + new String(pwInObject) +
-                    " returned from AppclientCommandArguments object",
-                    Arrays.equals(pwInObject, EXPECTED_PASSWORD_IN_PASSWORD_FILE.toCharArray()));
-
-        } catch (Throwable t) {
-            throw new RuntimeException(t);
-        }
+        props.setProperty(AppclientCommandArguments.PASSWORD_FILE_PASSWORD_KEYWORD, EXPECTED_PASSWORD_IN_PASSWORD_FILE);
+        final AppclientCommandArguments cmdArgs = prepareWithPWFile(props);
+        assertArrayEquals(EXPECTED_PASSWORD_IN_PASSWORD_FILE.toCharArray(), cmdArgs.getPassword(),
+            "Password in password file does not match expected password");
     }
-
-
 
     @Test
     public void checkErrorHandlingIfRequiredPasswordInPasswordFileIsMissing() {
         final Properties props = new Properties();
         props.setProperty("UNEXPECTED", EXPECTED_PASSWORD_IN_PASSWORD_FILE);
-        try {
-            final AppclientCommandArguments cmdArgs = prepareWithPWFile(props);
-            fail("Missing password in password file NOT correctly detected and flagged");
-        } catch (UserError ue) {
-            /*
-             * This is what we expect - a UserError complaining about the
-             * missing password value.
-             */
-            return;
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
+        assertThrows(UserError.class, () -> prepareWithPWFile(props),
+            "Missing password in password file NOT correctly detected and flagged");
     }
 
-    private AppclientCommandArguments prepareWithPWFile(
-            final Properties props) throws UserError, IOException {
+    private AppclientCommandArguments prepareWithPWFile(final Properties props) throws UserError, IOException {
         final File passwordFile = createTempPWFile(props);
-
-        return AppclientCommandArguments.newInstance(
-                Arrays.asList("-passwordfile","\"" + passwordFile.getAbsolutePath() + "\""));
+        return AppclientCommandArguments
+            .newInstance(Arrays.asList("-passwordfile", "\"" + passwordFile.getAbsolutePath() + "\""));
     }
 
     private File createTempPWFile(final Properties props) throws IOException {
@@ -180,6 +128,4 @@ public class AppclientCommandArgumentsTest {
         tempFile.deleteOnExit();
         return tempFile;
     }
-
-
 }

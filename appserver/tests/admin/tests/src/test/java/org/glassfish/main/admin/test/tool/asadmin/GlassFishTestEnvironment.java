c1f51c0c40bdd8610f1c4ddac845593947027290
David Matějček <dmatej@seznam.cz>
2022-04-13 08:13:39 +0200

    #23507 Using glassfish7 directory name

--- appserver/tests/admin/tests/src/test/java/org/glassfish/main/admin/test/tool/asadmin/GlassFishTestEnvironment.java
+++ appserver/tests/admin/tests/src/test/java/org/glassfish/main/admin/test/tool/asadmin/GlassFishTestEnvironment.java
@@ -147,7 +147,7 @@ public class GlassFishTestEnvironment {
 
 
     private static File resolveGlassFishRoot() {
-        final File gfDir = BASEDIR.toPath().resolve(Path.of("target", "glassfish6", "glassfish")).toFile();
+        final File gfDir = BASEDIR.toPath().resolve(Path.of("target", "glassfish7", "glassfish")).toFile();
         if (gfDir == null || !gfDir.exists()) {
             throw new IllegalStateException("The expected GlassFish home directory doesn't exist: " + gfDir);
         }

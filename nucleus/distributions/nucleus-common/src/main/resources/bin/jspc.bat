757ea75986ec947dc6c88c0ab72ea27e28af6aa9
Lukas Jungmann <lukas.jungmann@oracle.com>
2022-04-20 13:34:18 +0200

    fix jakartaee.jar path in jspc.bat/sh
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- nucleus/distributions/nucleus-common/src/main/resources/bin/jspc.bat
+++ nucleus/distributions/nucleus-common/src/main/resources/bin/jspc.bat
@@ -1,6 +1,6 @@
 @echo off
 REM
-REM  Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
+REM  Copyright (c) 2018, 2022 Oracle and/or its affiliates. All rights reserved.
 REM
 REM  This program and the accompanying materials are made available under the
 REM  terms of the Eclipse Public License v. 2.0, which is available at
@@ -22,6 +22,6 @@ set JSP_IMPL=%AS_INSTALL_LIB%\wasp.jar
 set EL_IMPL=%AS_INSTALL_LIB%\expressly;%AS_INSTALL_LIB%\jakarta.el-api.jar
 set JSTL_IMPL=%AS_INSTALL_LIB%\jakarta.servlet.jsp.jstl.jar
 set AS_LIB=%~dp0..\lib
-set JAVAEE_API=%AS_LIB%\javaee.jar
+set JAKARTAEE_API=%AS_LIB%\jakartaee.jar
 
-java -cp "%JSP_IMPL%;%JAVAEE_API%;%AS_LIB%" org.apache.jasper.JspC -sysClasspath "%JSP_IMPL%;%EL_IMPL%;%JSTL_IMPL%;%JAVAEE_API%;%AS_LIB%" -schemas "/schemas/" -dtds "/dtds/" %*
+java -cp "%JSP_IMPL%;%JAKARTAEE_API%;%AS_LIB%" org.apache.jasper.JspC -sysClasspath "%JSP_IMPL%;%EL_IMPL%;%JSTL_IMPL%;%JAKARTAEE_API%;%AS_LIB%" -schemas "/schemas/" -dtds "/dtds/" %*

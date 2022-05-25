eclipse-ee4j/glassfish

commit 8262ac43857d14c6186bd27009517a977df2073e

Fixed saving of temporary files (uploaded when deploying)
- previous state caused an unsupported archive exception

nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/utils/Util.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
  * Copyright (c) 2009, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -44,8 +45,6 @@ import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.HttpHeaders;
 import org.glassfish.admin.rest.Constants;
 import org.glassfish.admin.rest.RestLogging;
-import org.glassfish.admin.rest.model.ResponseBody;
-
 import org.glassfish.admin.rest.utils.xml.RestActionReporter;
 import org.glassfish.admin.restconnector.RestConfig;
 import org.glassfish.api.ActionReport.MessagePart;
@@ -330,7 +329,7 @@ public class Util {
     }
 
     public static Map<String, String> getCurrentValues(String basePath, ServiceLocator habitat, Subject subject) {
-        Map<String, String> values = new HashMap<String, String>();
+        Map<String, String> values = new HashMap<>();
         final String path = (basePath.endsWith(".")) ? basePath.substring(0, basePath.length() - 1) : basePath;
         RestActionReporter gr = ResourceUtil.runCommand("get", new ParameterMap() {
             {
@@ -442,7 +441,6 @@ public class Util {
             String sep = "";
             for (CommandModel.ParamModel model : params) {
                 Param param = model.getParam();
-                boolean include = true;
                 if (param.optional() && !includeOptional) {
                     continue;
                 }
@@ -467,38 +465,38 @@ public class Util {
         return sb.toString();
     }
 
-    public static File saveFile(String fileName, String mimeType, InputStream fileStream) {
-        BufferedOutputStream out = null;
-        File f = null;
+    public static File saveTemporaryFile(String fileName, InputStream fileStream) {
+        File file;
         try {
-            if (fileName.contains(".")) {
-                //String prefix = fileName.substring(0, fileName.indexOf("."));
-                // String suffix = fileName.substring(fileName.indexOf("."), fileName.length());
-                //if (prefix.length() < 3) {
-                //    prefix = "glassfish" + prefix;
-                //}
-                f = new File(new File(System.getProperty("java.io.tmpdir")), fileName);
-            }
-
-            out = new BufferedOutputStream(new FileOutputStream(f));
+            String[] parts = getNameAndSuffix(fileName);
+            file = File.createTempFile(parts[0], parts[1]);
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not create a temp file for " + fileName, e);
+        }
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
             byte[] buffer = new byte[32 * 1024];
             int bytesRead = 0;
             while ((bytesRead = fileStream.read(buffer)) != -1) {
                 out.write(buffer, 0, bytesRead);
             }
-            return f;
-        } catch (IOException ex) {
-            RestLogging.restLogger.log(Level.SEVERE, RestLogging.IO_EXCEPTION, ex.getMessage());
-        } finally {
-            try {
-                if (out != null) {
-                    out.close();
-                }
-            } catch (IOException ex) {
-                RestLogging.restLogger.log(Level.SEVERE, RestLogging.IO_EXCEPTION, ex.getMessage());
-            }
+            return file;
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not write to a temp file " + file, e);
         }
-        return null;
+    }
+
+
+    private static String[] getNameAndSuffix(String fileName) {
+        if (fileName == null) {
+            return new String[2];
+        }
+        int dotPosition = fileName.lastIndexOf('.');
+        if (dotPosition < 1) {
+            // on Linux: files with dots as the first char mean hidden files.
+            // or the dot wasn't found at all
+            return new String[] {fileName, null};
+        }
+        return new String[] {fileName.substring(0, dotPosition), fileName.substring(dotPosition)};
     }
 
     public static boolean isGenericType(Type type) {

nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/resources/TemplateCommandPostResource.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
  * Copyright (c) 2010, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -43,13 +44,9 @@ import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.sse.SseFeature;
 
-import static org.glassfish.admin.rest.resources.TemplateExecCommand.localStrings;
-
 /**
- *
  * @author ludovic champenois ludo@dev.java.net Code moved from generated classes to here. Gen code inherits from this
  * template class that contains the logic for mapped commands RS Resources
- *
  */
 @Produces({ "text/html", MediaType.APPLICATION_JSON + ";qs=0.5", MediaType.APPLICATION_XML + ";qs=0.5" })
 public class TemplateCommandPostResource extends TemplateExecCommand {
@@ -175,20 +172,18 @@ public class TemplateCommandPostResource extends TemplateExecCommand {
             formData = new FormDataMultiPart();
         }
         try {
-            /* data passed to the generic command running
-             *
-             * */
-
+            // data passed to the generic command running
             Map<String, List<FormDataBodyPart>> m1 = formData.getFields();
 
             Set<String> ss = m1.keySet();
             for (String fieldName : ss) {
                 for (FormDataBodyPart bodyPart : formData.getFields(fieldName)) {
 
-                    if (bodyPart.getContentDisposition().getFileName() != null) {//we have a file
+                    if (bodyPart.getContentDisposition().getFileName() == null) {
+                        data.add(fieldName, bodyPart.getValue());
+                    } else {
                         //save it and mark it as delete on exit.
                         InputStream fileStream = bodyPart.getValueAs(InputStream.class);
-                        String mimeType = bodyPart.getMediaType().toString();
 
                         //Use just the filename without complete path. File creation
                         //in case of remote deployment failing because fo this.
@@ -201,12 +196,9 @@ public class TemplateCommandPostResource extends TemplateExecCommand {
                             }
                         }
 
-                        File f = Util.saveFile(fileName, mimeType, fileStream);
-                        f.deleteOnExit();
+                        File f = Util.saveTemporaryFile(fileName, fileStream);
                         //put only the local path of the file in the same field.
                         data.add(fieldName, f.getAbsolutePath());
-                    } else {
-                        data.add(fieldName, bodyPart.getValue());
                     }
                 }
             }

nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/resources/TemplateRestResource.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
  * Copyright (c) 2009, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -86,7 +87,7 @@ public class TemplateRestResource extends AbstractResource implements OptionsCap
     protected ConfigModel childModel; //good model even if the child entity is null
     protected String childID; // id of the current child if part of a list, might be null
     public final static LocalStringManagerImpl localStrings = new LocalStringManagerImpl(TemplateRestResource.class);
-    final private static List<String> attributesToSkip = new ArrayList<String>() {
+    final private static List<String> attributesToSkip = new ArrayList<>() {
 
         {
             add("parent");
@@ -118,7 +119,7 @@ public class TemplateRestResource extends AbstractResource implements OptionsCap
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
 
-        return getAttributes((ConfigBean) getEntity());
+        return getAttributes(getEntity());
     }
 
     @POST
@@ -183,7 +184,7 @@ public class TemplateRestResource extends AbstractResource implements OptionsCap
      */
     protected RestActionReporter doCreateOrUpdate(HashMap<String, String> data) {
         if (data == null) {
-            data = new HashMap<String, String>();
+            data = new HashMap<>();
         }
         try {
             //data.remove("submit");
@@ -226,7 +227,7 @@ public class TemplateRestResource extends AbstractResource implements OptionsCap
 
     protected ExitCode doDelete(HashMap<String, String> data) {
         if (data == null) {
-            data = new HashMap<String, String>();
+            data = new HashMap<>();
         }
         if (entity == null) {//wrong resource
             //            return Response.status(404).entity(ResourceUtil.getActionReportResult(ActionReport.ExitCode.FAILURE, errorMessage, requestHeaders, uriInfo)).build();
@@ -372,7 +373,7 @@ public class TemplateRestResource extends AbstractResource implements OptionsCap
      * local location on the server side (ie. just the path)
      */
     public static HashMap<String, String> createDataBasedOnForm(FormDataMultiPart formData) {
-        HashMap<String, String> data = new HashMap<String, String>();
+        HashMap<String, String> data = new HashMap<>();
         try {
             //data passed to the generic command running
             Map<String, List<FormDataBodyPart>> m1 = formData.getFields();
@@ -380,10 +381,11 @@ public class TemplateRestResource extends AbstractResource implements OptionsCap
             Set<String> ss = m1.keySet();
             for (String fieldName : ss) {
                 for (FormDataBodyPart bodyPart : formData.getFields(fieldName)) {
-                    if (bodyPart.getContentDisposition().getFileName() != null) {//we have a file
+                    if (bodyPart.getContentDisposition().getFileName() == null) {
+                        data.put(fieldName, bodyPart.getValue());
+                    } else {
                         //save it and mark it as delete on exit.
                         InputStream fileStream = bodyPart.getValueAs(InputStream.class);
-                        String mimeType = bodyPart.getMediaType().toString();
 
                         //Use just the filename without complete path. File creation
                         //in case of remote deployment failing because fo this.
@@ -396,13 +398,9 @@ public class TemplateRestResource extends AbstractResource implements OptionsCap
                             }
                         }
 
-                        File f = Util.saveFile(fileName, mimeType, fileStream);
-                        f.deleteOnExit();
+                        File f = Util.saveTemporaryFile(fileName, fileStream);
                         //put only the local path of the file in the same field.
                         data.put(fieldName, f.getAbsolutePath());
-
-                    } else {
-                        data.put(fieldName, bodyPart.getValue());
                     }
                 }
             }
@@ -448,7 +446,7 @@ public class TemplateRestResource extends AbstractResource implements OptionsCap
 
                 String keyvalue = c.attribute(keyAttributeName.toLowerCase(Locale.US));
                 if (keyvalue.equals(childID)) {
-                    setEntity((ConfigBean) c);
+                    setEntity(c);
                 }
             }
         }
@@ -565,7 +563,7 @@ public class TemplateRestResource extends AbstractResource implements OptionsCap
 
     //******************************************************************************************************************
     private Map<String, String> getAttributes(Dom entity) {
-        Map<String, String> result = new TreeMap<String, String>();
+        Map<String, String> result = new TreeMap<>();
         Set<String> attributeNames = entity.model.getAttributeNames();
         for (String attributeName : attributeNames) {
             result.put(eleminateHypen(attributeName), entity.attribute(attributeName));
@@ -575,7 +573,7 @@ public class TemplateRestResource extends AbstractResource implements OptionsCap
     }
 
     private Map<String, MethodMetaData> getMethodMetaData() {
-        Map<String, MethodMetaData> map = new TreeMap<String, MethodMetaData>();
+        Map<String, MethodMetaData> map = new TreeMap<>();
         //GET meta data
         map.put("GET", new MethodMetaData());
 

commit 59f862ba03a437a363249289883cd1b7c618bb75

Fix of default context root with a generated suffix. This is caused by commit 8262ac43857d14c6186bd27009517a977df2073e which saves the uploaded deployment file to a temporary file, which automatically generates a suffix. Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@payara.fish>

nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/utils/Util.java
@@ -43,6 +43,8 @@ import javax.security.auth.Subject;
 import jakarta.ws.rs.core.PathSegment;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.HttpHeaders;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.glassfish.admin.rest.Constants;
 import org.glassfish.admin.rest.RestLogging;
 import org.glassfish.admin.rest.utils.xml.RestActionReporter;
@@ -468,8 +470,7 @@ public class Util {
     public static File saveTemporaryFile(String fileName, InputStream fileStream) {
         File file;
         try {
-            String[] parts = getNameAndSuffix(fileName);
-            file = File.createTempFile(parts[0], parts[1]);
+            file = Files.createTempDirectory("gf_uploads").resolve(fileName).toFile();
         } catch (IOException e) {
             throw new IllegalStateException("Could not create a temp file for " + fileName, e);
         }
@@ -479,6 +480,7 @@ public class Util {
             while ((bytesRead = fileStream.read(buffer)) != -1) {
                 out.write(buffer, 0, bytesRead);
             }
+            file.deleteOnExit();
             return file;
         } catch (IOException e) {
             throw new IllegalStateException("Could not write to a temp file " + file, e);

appserver/tests/admin/tests/src/test/java/org/glassfish/main/admin/test/rest/ApplicationITest.java
@@ -18,6 +18,7 @@
 package org.glassfish.main.admin.test.rest;
 
 import jakarta.ws.rs.core.Response;
+import java.io.File;
 
 import java.net.URISyntaxException;
 import java.util.Map;
@@ -66,6 +67,26 @@ public class ApplicationITest extends RestTestBase {
         }
     }
 
+    @Test
+    public void testApplicationDeploymentWithDefaultContextRoot() throws URISyntaxException {
+        try {
+            final File war = getWar("test");
+            String expectedContextRoot = getFileNameWithoutSuffix(war.getName());
+            Map<String, String> deployedApp = deployApp(war, null, appName);
+            assertEquals(appName, deployedApp.get("name"));
+            assertEquals("/" + expectedContextRoot, deployedApp.get("contextRoot"));
+        } finally {
+            undeployApp(appName);
+        }
+    }
+
+    private static String getFileNameWithoutSuffix(final String filename) {
+        if (filename.contains(".")) {
+            return filename.substring(0, filename.lastIndexOf("."));
+        } else {
+            return filename;
+        }
+    }
 
     @Test
     public void testApplicationDisableEnable() throws URISyntaxException {

appserver/tests/admin/tests/src/test/java/org/glassfish/main/admin/test/rest/RestTestBase.java
@@ -175,15 +175,24 @@ public class RestTestBase {
         assertEquals(404, response.getStatus());
     }
 
+    /*
+     * Arguments contextRoot and name can be null, then they are not set in the deploy command
+     */
     public Map<String, String> deployApp(final File archive, final String contextRoot, final String name) {
-        Map<String, Object> app = Map.of(
-            "id", archive,
-            "contextroot", contextRoot,
-            "name", name
-        );
+        Map<String, Object> app = new HashMap<>();
+        app.put("id", archive);
+        putIfNotNull("contextroot", contextRoot, app);
+        putIfNotNull("name", name, app);
+        
         Response response = managementClient.postWithUpload(URL_APPLICATION_DEPLOY, app);
         assertThat(response.getStatus(), equalTo(200));
         return getEntityValues(managementClient.get(URL_APPLICATION_DEPLOY + "/" + app.get("name")));
+    }
+
+    private void putIfNotNull(final String key, Object value, Map<String, Object> app) {
+        if (value != null) {
+            app.put(key, value);
+        }
     }
 
     public void addAppRef(final String applicationName, final String targetName){
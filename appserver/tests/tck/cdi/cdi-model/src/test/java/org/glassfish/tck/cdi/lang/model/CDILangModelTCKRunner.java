356e21db7974dbb1ce99cda80060d730653b0cd4
Arjan Tijms <arjan.tijms@gmail.com>
2022-07-08 16:56:43 +0200

    Add execution of the CDI Lang Model TCK
    Signed-off-by: Arjan Tijms <arjan.tijms@gmail.com>

---
+++ appserver/tests/tck/cdi/cdi-model/src/test/java/org/glassfish/tck/cdi/lang/model/CDILangModelTCKRunner.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2022, 2022 Contributors to the Eclipse Foundation. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package org.glassfish.tck.cdi.lang.model;
+
+import static org.glassfish.tck.cdi.lang.model.LangModelVerifierBuildCompatibleExtension.langModelVerifierBuildCompatibleExtensionPassed;
+import static org.jboss.shrinkwrap.api.BeanDiscoveryMode.ALL;
+import static org.junit.Assert.assertTrue;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.cdi.lang.model.tck.LangModelVerifier;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.impl.BeansXml;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import jakarta.enterprise.inject.build.compatible.spi.BuildCompatibleExtension;
+import jakarta.enterprise.inject.spi.Extension;
+
+@RunWith(Arquillian.class)
+public class CDILangModelTCKRunner {
+
+    @Deployment
+    public static WebArchive deploy() {
+        WebArchive archive= ShrinkWrap.create(WebArchive.class)
+
+                // The package we are testing
+                .addPackage(LangModelVerifier.class.getPackage())
+
+                // The build compatible extension that starts the test
+                .addClass(LangModelVerifierBuildCompatibleExtension.class)
+                .addAsServiceProvider(BuildCompatibleExtension.class, LangModelVerifierBuildCompatibleExtension.class)
+
+                // The cleanup extension that vetoes all classes that we're testing, since they are only
+                // meant for the lang model verifier and shouldn't be processed afterwards.
+                .addClass(CleanupExtension.class)
+                .addAsServiceProvider(Extension.class, CleanupExtension.class)
+
+                .addAsWebInfResource(new BeansXml(ALL), "beans.xml");
+
+        System.out.println(archive.toString(true));
+
+        return archive;
+
+    }
+
+    @Test
+    public void testCDILangModel() {
+        // If the extension has been called, and no exception was thrown
+        assertTrue(langModelVerifierBuildCompatibleExtensionPassed);
+    }
+}

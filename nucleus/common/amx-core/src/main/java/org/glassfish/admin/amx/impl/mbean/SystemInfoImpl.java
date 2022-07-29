6b4b1e5fb944472972dc46275e159dba5847516d
David Matějček <david.matejcek@omnifish.ee>
2022-07-29 21:02:54 +0200

    SystemInfoImpl - fixed javadoc
    - caused that Eclipse refused it's children

--- nucleus/common/amx-core/src/main/java/org/glassfish/admin/amx/impl/mbean/SystemInfoImpl.java
+++ nucleus/common/amx-core/src/main/java/org/glassfish/admin/amx/impl/mbean/SystemInfoImpl.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2022 Contributors to Eclipse Foundation.
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -27,31 +28,30 @@ import org.glassfish.admin.amx.base.SystemInfo;
 import org.glassfish.external.amx.AMXGlassfish;
 
 /**
-Loaded as MBean "amx:j2eeType=X-SystemInfo,name=na"
+ * Loaded as MBean <code>amx:j2eeType=X-SystemInfo,name=na</code>
  */
-public final class SystemInfoImpl extends AMXImplBase //implements SystemInfo
-{
+public final class SystemInfoImpl extends AMXImplBase {
+
     private final ConcurrentMap<String, Boolean> mFeatures;
 
-    public SystemInfoImpl(
-            final MBeanServer server) {
-
+    public SystemInfoImpl(final MBeanServer server) {
         super(AMXGlassfish.DEFAULT.domainRoot(), SystemInfo.class);
 
         // must be thread-safe, because features can be added at a later time
         mFeatures = new ConcurrentHashMap<String, Boolean>();
     }
 
-    /**
-    Advertise the presence of a feature.  For consistency, feature names should normally be
-    of the form <description>_FEATURE.  For example: "HELLO-WORLD_FEATURE".
-    <p>
-    To change a feature�s availability to unavailable, pass 'false' for 'available' (there is no
-    removeFeature() call).  This is discouraged unless dynamic presence/absence is an inherent
-    characteristic of the feature; clients might check only once for presence or absence.
 
-    @param featureName name of the feature
-    @param available  should be 'true' unless an explicit 'false' (unavailable) is desired
+    /**
+     * Advertise the presence of a feature. For consistency, feature names should normally be
+     * of the form <code>[description]_FEATURE</code>. For example: <code>HELLO-WORLD_FEATURE</code>
+     * <p>
+     * To change a feature's availability to unavailable, pass 'false' for 'available' (there is no
+     * removeFeature() call). This is discouraged unless dynamic presence/absence is an inherent
+     * characteristic of the feature; clients might check only once for presence or absence.
+     *
+     * @param featureName name of the feature
+     * @param available should be 'true' unless an explicit 'false' (unavailable) is desired
      */
     public void addFeature(final String featureName, final boolean available) {
         if (featureName == null || featureName.length() == 0) {
@@ -82,12 +82,13 @@ public final class SystemInfoImpl extends AMXImplBase //implements SystemInfo
         return (result);
     }
 
+
     /**
-    Return a Map keyed by an arbitrary String denoting some feature.  The value
-    is the time in milliseconds.  Code should not rely on the keys as they are subject to
-    changes, additions, or removal at any time, except as otherwise documented.
-    Even documented items should be used only for informational purposes,
-    such as assessing performance.
+     * Return a Map keyed by an arbitrary String denoting some feature. The value
+     * is the time in milliseconds. Code should not rely on the keys as they are subject to
+     * changes, additions, or removal at any time, except as otherwise documented.
+     * Even documented items should be used only for informational purposes,
+     * such as assessing performance.
      */
     public Map<String, Long> getPerformanceMillis() {
         return new HashMap<String, Long>();

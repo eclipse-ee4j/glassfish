cb1143e203052ccd2972323f2ed6af025a18d3a0
David Matějček <david.matejcek@omnifish.ee>
2022-08-09 16:01:24 +0200

    Issue #24070 Fixed thousands of cross references in the documentation
    - partially regexp changes, partially manual after asciidoctor validator
      reported inconsistencies
    
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

---
+++ docs/report-unused-refs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# each line means a set of ids of the same label
+# the last id is usually the most descriptive and should be used
+# the other should be removed
+ids=$(grep -h -o -r './' --include=\*.adoc --exclude-dir=target -e '^\(\[\[[0-9A-Za-z_\\-]\+\]\]\)\+' | tr -d "[" | tr -s "]]" ",")
+redundantIds="";
+for line in ${ids} ; do
+	IFS=','
+	labels=($line);
+	len=${#labels[@]};
+	if [[ $len != 1 ]] 
+	then
+		correctId=${labels[$len-1]};
+		maxIncorrectIdIndex=$(($len-2));
+#		echo "Max=$maxIncorrectIdIndex"
+#		echo "Correct id=${correctId}";
+		unset IFS;
+		for i in $(seq 0 $maxIncorrectIdIndex) ; do
+#			echo "Index: $i";
+			redundantId=${labels[$i]};
+			if [[ "$redundantIds" == *",$redundantId,"* ]]; then
+				echo "Duplicit id must be fixed first: ${redundantId}";
+				grep -o -r './' --include=\*.adoc --exclude-dir=target -e '^\(\[\[[0-9A-Za-z_\\-]\+\]\]\)\+' | grep "\[${redundantId}\]";
+				exit 1;
+			fi
+			redundantIds="${redundantIds},${redundantId},"
+			echo "Replacing $redundantId by $correctId";
+		done;
+#	else 
+#		echo "$labels is ok";
+	fi;
+done;
+
+

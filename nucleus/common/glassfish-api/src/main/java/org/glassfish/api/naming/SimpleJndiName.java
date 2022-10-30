5bb3150edf017fe7d91eb76a2b927a290759c0e6
David Matějček <david.matejcek@omnifish.ee>
2022-10-30 16:01:19 +0100

    JNDI: Introduced SimpleJndiName
    - targets
      - be able to separate common strings, names, ids, and JNDI names.
      - to provide some easy-to-use methods
      - to limit hacks manipulating these strings to get what desired
      - to be able to see mistakes or detect them by compiler
    
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

---
+++ nucleus/common/glassfish-api/src/main/java/org/glassfish/api/naming/SimpleJndiName.java
@@ -0,0 +1,235 @@
+/*
+ * Copyright (c) 2022 Eclipse Foundation and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package org.glassfish.api.naming;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.naming.CompositeName;
+import javax.naming.InvalidNameException;
+import javax.naming.Name;
+
+/**
+ * Value object for JNDI names used in GlassFish.
+ *
+ * @author David Matejcek
+ */
+public class SimpleJndiName implements Serializable, Comparable<SimpleJndiName> {
+
+    private static final long serialVersionUID = -6969478638009057579L;
+
+    public static final String JNDI_CTX_CORBA = "corbaname:";
+    public static final String JNDI_CTX_JAVA = "java:";
+    public static final String JNDI_CTX_JAVA_APP = JNDI_CTX_JAVA + "app/";
+    public static final String JNDI_CTX_JAVA_APP_ENV = JNDI_CTX_JAVA_APP + "env/";
+    public static final String JNDI_CTX_JAVA_COMPONENT = JNDI_CTX_JAVA + "comp/";
+    public static final String JNDI_CTX_JAVA_COMPONENT_ENV = JNDI_CTX_JAVA_COMPONENT + "env/";
+    public static final String JNDI_CTX_JAVA_MODULE = JNDI_CTX_JAVA + "module/";
+    public static final String JNDI_CTX_JAVA_MODULE_ENV = JNDI_CTX_JAVA_MODULE + "env/";
+    public static final String JNDI_CTX_JAVA_GLOBAL = JNDI_CTX_JAVA + "global/";
+
+    private final String jndiName;
+
+    /**
+     * Does simple validation and creates the instance.
+     *
+     * @param jndiName must not be null and if it is not a <code>corbaname:</code> jndi name, must
+     *            not contain more than one colon.
+     */
+    public SimpleJndiName(final String jndiName) {
+        if (!jndiName.startsWith(JNDI_CTX_CORBA)) {
+            int firstColon = jndiName.indexOf(':');
+            if (firstColon >= 0 && firstColon != jndiName.lastIndexOf(':')) {
+                throw new IllegalArgumentException(
+                    "Non-corba JNDI name is not allowed to contain more than one colon: " + jndiName);
+            }
+        }
+        this.jndiName = Objects.requireNonNull(jndiName, "jndiName");
+    }
+
+
+    /**
+     * @return true if the JNDI name starts with {@value #JNDI_CTX_JAVA}
+     */
+    public boolean hasJavaPrefix() {
+        return jndiName.startsWith(JNDI_CTX_JAVA);
+    }
+
+
+    /**
+     * @return true if the JNDI name starts with {@value #JNDI_CTX_CORBA}
+     */
+    public boolean hasCorbaPrefix() {
+        return jndiName.startsWith(JNDI_CTX_CORBA);
+    }
+
+
+    /**
+     * @return true if the JNDI name starts with {@value #JNDI_CTX_JAVA_GLOBAL} without the trailing slash.
+     */
+    public boolean isJavaGlobal() {
+        return jndiName.startsWith("java:global");
+    }
+
+
+    /**
+     * @return true if the JNDI name starts with {@value #JNDI_CTX_JAVA_APP} without the trailing slash.
+     */
+    public boolean isJavaApp() {
+        return jndiName.startsWith("java:app");
+    }
+
+
+    /**
+     * @return true if the JNDI name starts with {@value #JNDI_CTX_JAVA_MODULE} without the trailing slash.
+     */
+    public boolean isJavaModule() {
+        return jndiName.startsWith("java:module");
+    }
+
+
+    /**
+     * @return true if the JNDI name starts with {@value #JNDI_CTX_JAVA_COMPONENT} without the trailing slash.
+     */
+    public boolean isJavaComponent() {
+        return jndiName.startsWith("java:comp");
+    }
+
+
+    @Override
+    public int compareTo(SimpleJndiName other) {
+        return other == null ? -1 : this.jndiName.compareTo(other.jndiName);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return this.jndiName.hashCode();
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        return object instanceof SimpleJndiName && jndiName.equals(((SimpleJndiName) object).jndiName);
+    }
+
+
+    /**
+     * @return true if the JNDI name is an empty string
+     */
+    public boolean isEmpty() {
+        return jndiName.isEmpty();
+    }
+
+
+    /**
+     * @param part must not be null.
+     * @return true if the JNDI name contains the parameter.
+     */
+    public boolean contains(String part) {
+        return jndiName.contains(part);
+    }
+
+
+    /**
+     * @param prefix must not be null.
+     * @return true if the JNDI name starts with the parameter.
+     */
+    public boolean hasPrefix(String prefix) {
+        return jndiName.startsWith(prefix);
+    }
+
+
+    /**
+     * @param suffix must not be null.
+     * @return true if the JNDI name ends with the parameter.
+     */
+    public boolean hasSuffix(String suffix) {
+        return jndiName.endsWith(suffix);
+    }
+
+
+    /**
+     * @param prefix can be null, then returns this instance unchanged.
+     * @return new instance with the prefix removed or this instance unchanged
+     *         if the prefix was not present.
+     */
+    public SimpleJndiName removePrefix(String prefix) {
+        if (prefix == null) {
+            return this;
+        }
+        int index = jndiName.indexOf(prefix);
+        if (index >= 0) {
+            return new SimpleJndiName(jndiName.substring(index + prefix.length()));
+        }
+        return this;
+    }
+
+
+    /**
+     * @param suffix can be null, then returns this instance unchanged.
+     * @return new instance with the suffix removed or this instance unchanged
+     *         if the suffix was not present.
+     */
+    public SimpleJndiName removeSuffix(String suffix) {
+        if (suffix == null) {
+            return this;
+        }
+        int index = jndiName.lastIndexOf(suffix);
+        if (index >= 0) {
+            return new SimpleJndiName(jndiName.substring(0, index));
+        }
+        return this;
+    }
+
+
+    /**
+     * Returns the JNDI name.
+     */
+    // don't change, it is heavily used!
+    @Override
+    public String toString() {
+        return this.jndiName;
+    }
+
+
+    /**
+     * @return {@link CompositeName} representing the same JNDI name.
+     * @throws InvalidNameException if the conversions was not possible.
+     */
+    public Name toName() throws InvalidNameException {
+        return new CompositeName(jndiName);
+    }
+
+
+    /**
+     * @param name can be null, then returns null.
+     * @return null or a new instance of {@link SimpleJndiName}.
+     */
+    public static SimpleJndiName of(Name name) {
+        return name == null ? null : new SimpleJndiName(name.toString());
+    }
+
+
+    /**
+     * @param name can be null, then returns null.
+     * @return null or a new instance of {@link SimpleJndiName}.
+     */
+    public static SimpleJndiName of(String name) {
+        return name == null ? null : new SimpleJndiName(name);
+    }
+}
---
+++ nucleus/common/glassfish-api/src/test/java/org/glassfish/api/naming/SimpleJndiNameTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2022 Eclipse Foundation and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package org.glassfish.api.naming;
+
+import javax.naming.CompositeName;
+import javax.naming.Name;
+
+import org.junit.jupiter.api.Test;
+
+import static org.glassfish.api.naming.SimpleJndiName.JNDI_CTX_JAVA_MODULE;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author David Matejcek
+ */
+public class SimpleJndiNameTest {
+
+    @Test
+    public void constructorValidations() {
+        assertAll(
+            () -> assertThrows(NullPointerException.class, () -> new SimpleJndiName(null)),
+            () -> assertThrows(IllegalArgumentException.class, () -> new SimpleJndiName("x:y:z")),
+            () -> assertDoesNotThrow(() -> new SimpleJndiName(""))
+        );
+    }
+
+
+    @Test
+    public void comparisons() {
+        assertAll(
+            () -> assertEquals(0, SimpleJndiName.of("xxx/yyy").compareTo(SimpleJndiName.of("xxx/yyy"))),
+            () -> assertEquals(-1, SimpleJndiName.of("xxx").compareTo(SimpleJndiName.of((String) null))),
+            () -> assertEquals(-1, SimpleJndiName.of("xxx").compareTo(SimpleJndiName.of((Name) null))),
+            () -> assertTrue(SimpleJndiName.of("a/b/c").contains("b/c")),
+            () -> assertEquals(SimpleJndiName.of("xxx").hashCode(), SimpleJndiName.of("xxx").hashCode()),
+            () -> assertTrue(SimpleJndiName.of("xxx").equals(SimpleJndiName.of(new CompositeName("xxx")))),
+            () -> assertTrue(SimpleJndiName.of("xxx/yyy").equals(SimpleJndiName.of("xxx/yyy")))
+        );
+    }
+
+
+    @Test
+    public void java() {
+        String jndiNameString = "java:x/y";
+        SimpleJndiName jndiName = SimpleJndiName.of(jndiNameString);
+        assertAll(
+            () -> assertFalse(jndiName.isEmpty()),
+            () -> assertFalse(jndiName.hasCorbaPrefix()),
+            () -> assertTrue(jndiName.hasJavaPrefix()),
+            () -> assertTrue(jndiName.hasPrefix("java:")),
+            () -> assertFalse(jndiName.hasSuffix("java:")),
+            () -> assertFalse(jndiName.isJavaApp()),
+            () -> assertFalse(jndiName.isJavaGlobal()),
+            () -> assertFalse(jndiName.isJavaComponent()),
+            () -> assertFalse(jndiName.isJavaModule()),
+            () -> assertEquals(jndiNameString, jndiName.toString()),
+            () -> assertEquals(new CompositeName(jndiNameString), jndiName.toName()),
+            () -> assertEquals(0, new SimpleJndiName(jndiNameString).compareTo(jndiName)),
+            () -> assertFalse(jndiName.isEmpty())
+        );
+    }
+
+
+    @Test
+    public void javaSubContexts() {
+        assertAll(
+            () -> assertTrue(SimpleJndiName.of("java:app/x").isJavaApp()),
+            () -> assertTrue(SimpleJndiName.of("java:comp/x").isJavaComponent()),
+            () -> assertTrue(SimpleJndiName.of("java:comp/env/x").isJavaComponent()),
+            () -> assertTrue(SimpleJndiName.of("java:module/x").isJavaModule()),
+            () -> assertTrue(SimpleJndiName.of("java:global/x").isJavaGlobal())
+        );
+    }
+
+
+    @Test
+    public void removals() {
+        SimpleJndiName jndiName = SimpleJndiName.of("java:module/x/y/z");
+        assertAll(
+            () -> assertEquals(SimpleJndiName.of("x/y/z"), jndiName.removePrefix(JNDI_CTX_JAVA_MODULE)),
+            () -> assertEquals(jndiName, jndiName.removePrefix("notThere")),
+            () -> assertEquals(jndiName, jndiName.removePrefix(null)),
+            () -> assertEquals(SimpleJndiName.of("java:module/x/"), jndiName.removeSuffix("y/z")),
+            () -> assertEquals(jndiName, jndiName.removeSuffix("notThere")),
+            () -> assertEquals(jndiName, jndiName.removeSuffix(null))
+        );
+    }
+
+
+    @Test
+    public void corba() {
+        String jndiNameString = "corbaname:x:y";
+        SimpleJndiName jndiName = SimpleJndiName.of(jndiNameString);
+        assertAll(
+            () -> assertFalse(jndiName.isEmpty()),
+            () -> assertTrue(jndiName.hasCorbaPrefix()),
+            () -> assertFalse(jndiName.hasJavaPrefix()),
+            () -> assertTrue(jndiName.hasPrefix("corbaname:")),
+            () -> assertTrue(jndiName.hasSuffix(":y")),
+            () -> assertFalse(jndiName.isJavaApp()),
+            () -> assertFalse(jndiName.isJavaGlobal()),
+            () -> assertFalse(jndiName.isJavaComponent()),
+            () -> assertFalse(jndiName.isJavaModule()),
+            () -> assertEquals(jndiNameString, jndiName.toString()),
+            () -> assertEquals(new CompositeName(jndiNameString), jndiName.toName()),
+            () -> assertEquals(0, new SimpleJndiName(jndiNameString).compareTo(jndiName)),
+            () -> assertFalse(jndiName.isEmpty())
+        );
+    }
+}

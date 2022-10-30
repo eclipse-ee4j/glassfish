ee953bcb3436feddb6c00a6193854aa4addcce02
David Matějček <david.matejcek@omnifish.ee>
2022-10-30 18:47:46 +0100

    Removed obsoleted target parameter for delete/create-jdbc-resource
    - it was obsoleted for many years (20?)
    - confusing
    
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- appserver/jdbc/admin/src/main/java/org/glassfish/jdbc/admin/cli/DeleteJdbcResource.java
+++ appserver/jdbc/admin/src/main/java/org/glassfish/jdbc/admin/cli/DeleteJdbcResource.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -19,23 +20,31 @@ package org.glassfish.jdbc.admin.cli;
 import com.sun.enterprise.config.serverbeans.Domain;
 import com.sun.enterprise.config.serverbeans.Resources;
 import com.sun.enterprise.util.LocalStringManagerImpl;
-import com.sun.enterprise.util.SystemPropertyConstants;
+
+import jakarta.inject.Inject;
+
 import org.glassfish.api.ActionReport;
 import org.glassfish.api.I18n;
 import org.glassfish.api.Param;
-import org.glassfish.api.admin.*;
+import org.glassfish.api.admin.AdminCommand;
+import org.glassfish.api.admin.AdminCommandContext;
+import org.glassfish.api.admin.ExecuteOn;
+import org.glassfish.api.admin.RestEndpoint;
+import org.glassfish.api.admin.RestEndpoints;
+import org.glassfish.api.admin.RuntimeType;
+import org.glassfish.api.naming.SimpleJndiName;
 import org.glassfish.config.support.CommandTarget;
 import org.glassfish.config.support.TargetType;
 import org.glassfish.hk2.api.PerLookup;
 import org.glassfish.resourcebase.resources.api.ResourceStatus;
 import org.jvnet.hk2.annotations.Service;
 
-import jakarta.inject.Inject;
+import static com.sun.enterprise.util.SystemPropertyConstants.DAS_SERVER_NAME;
 
 /**
  * Delete JDBC Resource command
- *
  */
+// FIXME: should be removed here, am I right or wrong?
 @TargetType(value={CommandTarget.DAS,CommandTarget.DOMAIN, CommandTarget.CLUSTER, CommandTarget.STANDALONE_INSTANCE })
 @RestEndpoints({
         @RestEndpoint(configBean = Resources.class,
@@ -50,19 +59,16 @@ import jakarta.inject.Inject;
 @I18n("delete.jdbc.resource")
 public class DeleteJdbcResource implements AdminCommand {
 
-    final private static LocalStringManagerImpl localStrings = new LocalStringManagerImpl(DeleteJdbcResource.class);
+    private static final LocalStringManagerImpl localStrings = new LocalStringManagerImpl(DeleteJdbcResource.class);
 
-    @Param(optional=true)
-    private String target = SystemPropertyConstants.DAS_SERVER_NAME;
-
-    @Param(name="jdbc_resource_name", primary=true)
+    @Param(name = "jdbc_resource_name", primary = true)
     private String jndiName;
 
     @Inject
     private Domain domain;
 
     @Inject
-    private JDBCResourceManager jdbcResMgr ;
+    private JDBCResourceManager jdbcResMgr;
 
     /**
      * Executes the command with the command parameters passed as Properties
@@ -70,11 +76,12 @@ public class DeleteJdbcResource implements AdminCommand {
      *
      * @param context information
      */
+    @Override
     public void execute(AdminCommandContext context) {
 
         final ActionReport report = context.getActionReport();
         try {
-            ResourceStatus rs = jdbcResMgr.delete(domain.getResources(), jndiName, target);
+            ResourceStatus rs = jdbcResMgr.delete(domain.getResources(), SimpleJndiName.of(jndiName), DAS_SERVER_NAME);
             if(rs.getMessage() != null){
                 report.setMessage(rs.getMessage());
             }
@@ -82,8 +89,9 @@ public class DeleteJdbcResource implements AdminCommand {
                 report.setActionExitCode(ActionReport.ExitCode.SUCCESS);
             } else {
                 report.setActionExitCode(ActionReport.ExitCode.FAILURE);
-                if (rs.getException() != null)
+                if (rs.getException() != null) {
                     report.setFailureCause(rs.getException());
+                }
             }
         } catch(Exception e) {
             report.setMessage(localStrings.getLocalString("delete.jdbc.resource.fail", "{0} delete failed ", jndiName));

79892f9f453430c770863b253e1a38f3fb2aa37b
Ondro Mihalyi <mihalyi@omnifish.ee>
2023-04-20 12:56:20 +0200

    Refactor ScatteredArchive tests For better readability Signed-off-by:Ondro Mihalyi <mihalyi@omnifish.ee>

--- nucleus/common/scattered-archive-api/src/test/java/org/glassfish/embeddable/archive/ScatteredArchiveTest.java
+++ nucleus/common/scattered-archive-api/src/test/java/org/glassfish/embeddable/archive/ScatteredArchiveTest.java
@@ -9,17 +9,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ArgumentConverter;
+import org.junit.jupiter.params.converter.ConvertWith;
 
 /**
  *
@@ -27,7 +30,7 @@ import static org.junit.jupiter.api.Assertions.assertSame;
  */
 public class ScatteredArchiveTest {
 
-    private final String REGEX_COMMA_WITH_SPACE_AROUND = "[ ]*,[ ]*";
+    private static final String REGEX_COMMA_WITH_SPACE_AROUND = "[ ]*,[ ]*";
 
     public ScatteredArchiveTest() {
     }
@@ -49,51 +52,55 @@ public class ScatteredArchiveTest {
 
     @ParameterizedTest
     @CsvSource({
-                "path, '', 1",
-                "path:path2, '', 2",
-                "'jakarta.jakarta-api.jar:/folder/path2:/folder/glassfish-embedded-all.jar', '', 1",
-                "'jakarta.jakarta-api.jar:glassfish-embedded-all.jar', '', 0",
-                "path, 'path', 0",
-                "path111:/folder/path222, 'path.*', 0",
-                "path333:path111:/folder/path222, 'path1.* , path2.*', 1",
-                "'jakarta.jakarta-api.jar:path2:/some/folder/path3:somepath', 'path.', 1",
-                "'', '', 0"
-            })
-    public void addCurrentClasspathUsingExcludes(String classpath, String excludesList, int expectedElementsInClasspath) {
+                "path, '', path",
+                "path:path2, '', 'path,path2'",
+                "'jakarta.jakarta-api.jar:/folder/path2:/folder/glassfish-embedded-all.jar', '', /folder/path2",
+                "'jakarta.jakarta-api.jar:glassfish-embedded-all.jar', '', ''",
+                "path, 'path', ''",
+                "path111:/folder/path222, path.*, ''",
+                "path333:path111:/folder/path222, 'path1.* , path2.*', path333",
+                "jakarta.jakarta-api.jar:path2:/some/folder/path3:somepath, path., somepath",
+                "'', '', ''"
+    })
+    public void addCurrentClasspathUsingExcludes(String classpath, 
+            @ConvertWith(ConvertCSVToArrayOfStrings.class) String[] excludesPatterns, 
+            @ConvertWith(ConvertCSVToListOfFiles.class) List<File> expectedFilesInClasspath) {
 
         // GIVEN
         setCurrentClasspath(classpath);
-        String[] excludesPatterns = excludesList.split(REGEX_COMMA_WITH_SPACE_AROUND);
         TestableScatteredArchive archive = new TestableScatteredArchive("test", ScatteredArchive.Type.WAR);
 
         // WHEN
         archive.addCurrentClassPath(excludesPatterns);
 
         // THEN
-        assertAll("classpath",
+        assertAll("Classpath",
                 () -> assertThat("Number of classpath elements",
-                        archive.classpathElements, hasSize(expectedElementsInClasspath))
+                        archive.classpathElements, hasSize(expectedFilesInClasspath.size())),
+                () -> assertThat("Classpath elements", archive.classpathElements, equalTo(expectedFilesInClasspath))
         );
     }
 
     @ParameterizedTest
     @CsvSource({
-        "path, '', 1",
-        "path:path2, '', 2",
-        "'jakarta.jakarta-api.jar:/folder/path2:/folder/glassfish-embedded-all.jar', '0', 2",
-        "'jakarta.jakarta-api.jar:glassfish-embedded-all.jar', '0,1', 0",
-        "path, '0', 0",
-        "path111:/folder/path222, '0,1', 0",
-        "path111:/folder/path222, '0', 1",
-        "'jakarta.jakarta-api.jar:path2:/some/folder/path3:somepath', '1,2', 2",
-        "'', '', 0"
+        "path, '', path",
+        "path:path2, '', 'path,path2'",
+        "'jakarta.jakarta-api.jar:/folder/path2:/folder/glassfish-embedded-all.jar', '0', '/folder/path2,/folder/glassfish-embedded-all.jar'",
+        "'jakarta.jakarta-api.jar:glassfish-embedded-all.jar', '0,1', ''",
+        "path, '0', ''",
+        "path111:/folder/path222, '0,1', ''",
+        "path111:/folder/path222, '0', /folder/path222",
+        "'jakarta.jakarta-api.jar:path2:/some/folder/path3:somepath', '1,2', 'jakarta.jakarta-api.jar,somepath'",
+        "'', '', ''"
     })
-    public void addCurrentClasspathUsingPredicate(String classpath, String excludesIndexes, int expectedElementsInClasspath) {
+    public void addCurrentClasspathUsingPredicate(String classpath, 
+            @ConvertWith(ConvertCSVToArrayOfStrings.class) String[] excludesIndexes, 
+            @ConvertWith(ConvertCSVToListOfFiles.class) List<File> expectedFilesInClasspath) {
 
         // GIVEN
         var cpElements = setCurrentClasspath(classpath);
         Predicate<String> exclude = value -> {
-            return Stream.of(excludesIndexes.split(REGEX_COMMA_WITH_SPACE_AROUND))
+            return Stream.of(excludesIndexes)
                     .filter(s -> !s.isBlank())
                     .map(Integer::valueOf)
                     .anyMatch(i -> cpElements[i].equals(value));
@@ -104,9 +111,10 @@ public class ScatteredArchiveTest {
         archive.addCurrentClassPath(exclude);
 
         // THEN
-        assertAll("classpath",
-                () -> assertThat("Number of classpath elements " + archive.classpathElements,
-                        archive.classpathElements, hasSize(expectedElementsInClasspath))
+        assertAll("Classpath",
+                () -> assertThat("Number of classpath elements",
+                        archive.classpathElements, hasSize(expectedFilesInClasspath.size())),
+                () -> assertThat("Classpath elements", archive.classpathElements, equalTo(expectedFilesInClasspath))
         );
     }
 
@@ -115,5 +123,41 @@ public class ScatteredArchiveTest {
         System.setProperty(ScatteredArchive.JAVA_CLASS_PATH_PROPERTY_KEY, osSpecificClasspath);
         return osSpecificClasspath.split("\\" + File.pathSeparator);
     }
+
+    private static class ConvertCSVToArrayOfStrings implements ArgumentConverter {
+
+        @Override
+        public Object convert(Object source, ParameterContext context)
+                throws ArgumentConversionException {
+            return convertToArrayOfStrings(source);
+        }
+
+        protected String[] convertToArrayOfStrings(Object source) throws IllegalArgumentException {
+            if (!(source instanceof String)) {
+                throw new IllegalArgumentException(
+                        "The argument should be a string: " + source);
+            }
+            try {
+                final String sourceString = (String) source;
+                return !sourceString.isBlank() ? sourceString.split(REGEX_COMMA_WITH_SPACE_AROUND) : new String[0];
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Failed to convert value " + source, e);
+            }
+        }
+    }
+    
+    private static class ConvertCSVToListOfFiles extends ConvertCSVToArrayOfStrings implements ArgumentConverter {
+
+        @Override
+        public Object convert(Object source, ParameterContext context)
+                throws ArgumentConversionException {
+            return List.of(this.convertToArrayOfStrings(source)).stream()
+                    .map(File::new)
+                    .collect(Collectors.toList());
+        }
+
+        
+    }
+    
 
 }

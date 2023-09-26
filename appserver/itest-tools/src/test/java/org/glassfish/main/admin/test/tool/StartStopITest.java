31d4f677c52284ab60b7cff9426ff8e640817231
David Matějček <david.matejcek@omnifish.ee>
2023-09-26 02:06:34 +0200

    Fixed StartStopITest - Windows vs. * asterisk - attempt2
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- appserver/itest-tools/src/test/java/org/glassfish/main/admin/test/tool/StartStopITest.java
+++ appserver/itest-tools/src/test/java/org/glassfish/main/admin/test/tool/StartStopITest.java
@@ -19,23 +19,33 @@ package org.glassfish.main.admin.test.tool;
 import org.glassfish.main.itest.tools.GlassFishTestEnvironment;
 import org.glassfish.main.itest.tools.asadmin.Asadmin;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static org.glassfish.main.itest.tools.asadmin.AsadminResultMatcher.asadminOK;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
+ * {@link GlassFishTestEnvironment} starts the domain, we execute the command and then
+ * {@link GlassFishTestEnvironment} stops the server automatically.
+ *
  * @author David Matejcek
  */
 class StartStopITest {
 
     private static final Asadmin ASADMIN = GlassFishTestEnvironment.getAsadmin();
 
-    /**
-     * {@link GlassFishTestEnvironment} starts the domain, we execute the command and then
-     * {@link GlassFishTestEnvironment} stops the server automatically.
-     */
     @Test
-    void asadminGet() {
-        assertThat(ASADMIN.exec("get", "'*'"), asadminOK());
+    @DisabledOnOs(OS.WINDOWS)
+    void asadminGet_Linux() {
+        assertThat(ASADMIN.exec("get", "*"), asadminOK());
+    }
+
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void asadminGet_Windows() {
+        assertThat(ASADMIN.exec("get", "\"*\""), asadminOK());
     }
 }

94b51ac23e74e8d48eefc8c770abd0170c546a22
Ondro Mihalyi <mihalyi@omnifish.ee>
2023-12-30 09:00:55 +0100

    A custom view handler that can delegate to default view handler Signed-off-by:Ondro Mihalyi <mihalyi@omnifish.ee>

---
+++ appserver/admingui/war/src/main/java/org/glassfish/admingui/common/AdminGuiViewHandler.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ * Copyright (c) 2010, 2018 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package org.glassfish.admingui.common;
+
+import jakarta.faces.application.Application;
+import jakarta.faces.application.ViewHandler;
+import jakarta.faces.application.ViewHandlerWrapper;
+import jakarta.faces.context.FacesContext;
+
+/**
+ *
+ * @author Ondro Mihalyi
+ */
+public class AdminGuiViewHandler extends ViewHandlerWrapper {
+
+    private ViewHandler defauViewHandler = null;
+
+    public AdminGuiViewHandler(ViewHandler wrapped) {
+        super(wrapped);
+        Application app = FacesContext.getCurrentInstance().getApplication();
+        if (app instanceof AdminGuiApplication) {
+            defauViewHandler = ((AdminGuiApplication) app).getDefaViewHandler();
+        } else {
+            defauViewHandler = wrapped;
+        }
+    }
+
+}

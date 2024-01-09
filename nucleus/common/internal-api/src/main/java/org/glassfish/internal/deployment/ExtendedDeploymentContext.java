e0e5f457eae42f4e00c156dc7dddaa8c40665050
Alexander Pinčuk <alexander.v.pinchuk@gmail.com>
2024-01-09 19:28:22 +0300

    Code cleanup
    Signed-off-by: Alexander Pinčuk <alexander.v.pinchuk@gmail.com>

--- nucleus/common/internal-api/src/main/java/org/glassfish/internal/deployment/ExtendedDeploymentContext.java
+++ nucleus/common/internal-api/src/main/java/org/glassfish/internal/deployment/ExtendedDeploymentContext.java
@@ -17,185 +17,183 @@
 
 package org.glassfish.internal.deployment;
 
-import java.io.File;
-import java.io.IOException;
 import org.glassfish.api.deployment.DeploymentContext;
 import org.glassfish.api.deployment.archive.ArchiveHandler;
 import org.glassfish.api.deployment.archive.ReadableArchive;
 import org.glassfish.internal.api.ClassLoaderHierarchy;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.util.Map;
 import java.util.Properties;
-import java.net.URISyntaxException;
-import java.net.MalformedURLException;
 
 /**
- * semi-private interface to the deployment context
+ * Semi-private interface to the deployment context.
  *
  * @author Jerome Dochez
  */
 public interface ExtendedDeploymentContext extends DeploymentContext {
 
-    public enum Phase { UNKNOWN, PREPARE, PREPARED, LOAD, START, STOP, UNLOAD, CLEAN, REPLICATION }
+    enum Phase { UNKNOWN, PREPARE, PREPARED, LOAD, START, STOP, UNLOAD, CLEAN, REPLICATION }
 
-    public static final String IS_TEMP_CLASSLOADER = "isTempClassLoader";
-    public static final String TRACKER = "tracker";
+    String IS_TEMP_CLASSLOADER = "isTempClassLoader";
+
+    String TRACKER = "tracker";
 
 
     /**
      * Sets the phase of the deployment activity.
      *
-     * @param newPhase
+     * @param newPhase the phase of the deployment activity
      */
-    public void setPhase(Phase newPhase);
+    void setPhase(Phase newPhase);
 
-    public Phase getPhase();
+    Phase getPhase();
 
     /**
-     * Create the deployment class loader. It will be used for sniffer
-     * retrieval, metadata parsing and deployer prepare.
+     * Create the deployment class loader.
+     *
+     * <p>It will be used for sniffer retrieval, metadata parsing and deployer prepare.
      *
      * @param clh the hierarchy of class loader for the parent
      * @param handler the archive handler for the source archive
      */
-    public void createDeploymentClassLoader(ClassLoaderHierarchy clh, ArchiveHandler handler)
-            throws URISyntaxException, MalformedURLException;
+    void createDeploymentClassLoader(ClassLoaderHierarchy clh, ArchiveHandler handler) throws URISyntaxException, MalformedURLException;
 
     /**
-     * Create the final class loader. It will be used to load and start
-     * application.
+     * Create the final class loader.
+     *
+     * <p>It will be used to load and start application.
      *
      * @param clh the hierarchy of class loader for the parent
      * @param handler the archive handler for the source archive
      */
-    public void createApplicationClassLoader(ClassLoaderHierarchy clh, ArchiveHandler handler)
-            throws URISyntaxException, MalformedURLException;
+    void createApplicationClassLoader(ClassLoaderHierarchy clh, ArchiveHandler handler) throws URISyntaxException, MalformedURLException;
 
-    public void clean();
+    void clean();
 
     /**
-     * Sets the archive handler that's associated with this context
+     * Sets the archive handler that's associated with this context.
      *
-     * @param archiveHandler
+     * @param archiveHandler the archive handler associated with this context
      */
-    public void setArchiveHandler(ArchiveHandler archiveHandler);
+    void setArchiveHandler(ArchiveHandler archiveHandler);
 
     /**
-     * Sets the source archive
+     * Sets the source archive.
      *
-     * @param props
+     * @param source the source archive
      */
-    public void setSource(ReadableArchive source);
+    void setSource(ReadableArchive source);
 
     /**
-     * Sets the module properties for modules
+     * Sets the module properties for modules.
      *
-     * @param modulePropsMap
+     * @param modulePropsMap the module properties map
      */
-    public void setModulePropsMap(Map<String, Properties> modulePropsMap);
+    void setModulePropsMap(Map<String, Properties> modulePropsMap);
 
     /**
-     * Gets the deployment context for modules
+     * Gets the deployment context for modules.
      *
      * @return a map containing module deployment contexts
      */
-    public Map<String, ExtendedDeploymentContext> getModuleDeploymentContexts();
+    Map<String, ExtendedDeploymentContext> getModuleDeploymentContexts();
 
    /**
-     * Sets the classloader
+     * Sets the classloader.
      *
-     * @param cloader
+     * @param classLoader the classloader
      */
-    public void setClassLoader(ClassLoader cloader);
+    void setClassLoader(ClassLoader classLoader);
 
    /**
-     * Sets the parent context
+     * Sets the parent context.
      *
-     * @param parentContext
+     * @param parentContext the parent context
      */
-    public void setParentContext(ExtendedDeploymentContext parentContext);
+    void setParentContext(ExtendedDeploymentContext parentContext);
 
     /**
-     * Gets the module uri for this module context
+     * Gets the module uri for this module context.
      *
      * @return the module uri
      */
-    public String getModuleUri();
+    String getModuleUri();
 
    /**
-     * Sets the module uri for this module context
+     * Sets the module uri for this module context.
      *
-     * @param moduleUri
+     * @param moduleUri the module uri
      */
-    public void setModuleUri(String moduleUri);
+    void setModuleUri(String moduleUri);
 
     /**
-     * Gets the parent context for this context
+     * Gets the parent context for this context.
      *
      * @return the parent context
      */
-    public ExtendedDeploymentContext getParentContext();
+    ExtendedDeploymentContext getParentContext();
 
 
     /**
-     * Returns the internal directory for the application (used for holding
-     * the uploaded archive, for example).
+     * Returns the internal directory for the application (used for holding the uploaded archive,
+     * for example).
      *
      * @return location of the internal directory for the application
      */
-    public File getAppInternalDir();
+    File getAppInternalDir();
 
     /**
-     * Returns the alternate deployment descriptor directory for the
-     * application (used for holding the external alternate deployment
-     * descriptors).
+     * Returns the alternate deployment descriptor directory for the application (used for holding
+     * the external alternate deployment descriptors).
      *
-     * @return location of the alternate deployment descriptor directory for
-     *  the application
+     * @return location of the alternate deployment descriptor directory for the application
      */
-    public File getAppAltDDDir();
+    File getAppAltDDDir();
 
 
     /**
-     * Returns the tenant, if one is valid for this DeploymentContext.
-     * @return tenant name if applicable, null if no tenant is set for this DC
-     */
-    public String getTenant();
-
-    /**
-     * Sets the tenant to which this deployment context applies. Also initializes
-     * the tenant directory.
+     * Returns the tenant, if one is valid for this {@code DeploymentContext}.
      *
-     * @param tenantName the name of the tenant
+     * @return tenant if applicable, {@code null} if no tenant is set for this deployment context
+     */
+    String getTenant();
+
+    /**
+     * Sets the tenant to which this deployment context applies.
+     *
+     * <p>Also initializes the tenant directory.
+     *
+     * @param tenant the name of the tenant
      * @param appName the name of the application
      */
-    public void setTenant(String tenant, String appName);
+    void setTenant(String tenant, String appName);
 
     /**
-     * Returns the directory containing the expanded tenant customization archive,
-     * if this DC is for a tenant and if a customization archive was specified
-     * when the tenant was provisioned.
-     * @return directory containing the expanded customization archive; null if none
-     */
-    public File getTenantDir();
-
-    /**
-     * Performs any clean-up of the deployment context after deployment has
-     * finished.
-     * <p>
-     * This method can be invoked either with "true", meaning that this is the
-     * final clean-up for the DC, or with "false," meaning that the DC
-     * implementation should be selective.  (Some data is used, for instance,
-     * in the DeployCommand logic after ApplicationLifeCycle.deploy has
-     * completed.)
+     * Returns the directory containing the expanded tenant customization archive, if this deployment context
+     * is for a tenant and if a customization archive was specified when the tenant was provisioned.
      *
-     * @param isFinalClean whether this clean is the final clean or a selective one.
+     * @return directory containing the expanded customization archive, {@code null} if none
      */
-    public void postDeployClean(boolean isFinalClean);
+    File getTenantDir();
 
     /**
-     * Prepare the scratch directories, creating the directories
-     * if they do not exist
+     * Performs any clean-up of the deployment context after deployment has finished.
+     *
+     * <p>This method can be invoked either with {@code true}, meaning that this is the final clean-up
+     * for the deployment context, or with {@code false}, meaning that the deployment context
+     * implementation should be selective.  (Some data is used, for instance,in the {@code DeployCommand}
+     * logic after {@code ApplicationLifeCycle.deploy()} has completed).
+     *
+     * @param isFinalClean whether this clean is the final clean or a selective one
      */
-    public void prepareScratchDirs() throws IOException;
+    void postDeployClean(boolean isFinalClean);
+
+    /**
+     * Prepare the scratch directories, creating the directories if they do not exist.
+     */
+    void prepareScratchDirs() throws IOException;
 }

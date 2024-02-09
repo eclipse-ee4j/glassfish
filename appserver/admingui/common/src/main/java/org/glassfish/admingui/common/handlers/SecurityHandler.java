c6ab6fbbaa64c9741a9b3644bedcdba354e47b4a
kaido207 <kaido.hiroki@fujitsu.com>
2024-02-09 19:33:31 +0900

    Fixes Unnecessary escaping
    Signed-off-by: kaido207 <kaido.hiroki@fujitsu.com>

--- appserver/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/SecurityHandler.java
+++ appserver/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/SecurityHandler.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation.
  * Copyright (c) 2009, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -277,7 +278,7 @@ public class SecurityHandler {
                 continue;
             }
             sb.append(oneProp.get("name")).append("=");
-            String value = ((String) oneProp.get("value")).replaceAll("\\\\", "\\\\\\\\");
+            String value = ((String) oneProp.get("value"));
             value = UtilHandlers.escapePropertyValue(value);
             sb.append(value).append(":");
         }

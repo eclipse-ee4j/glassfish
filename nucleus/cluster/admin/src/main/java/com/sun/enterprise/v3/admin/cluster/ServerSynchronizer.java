1d05944060dbe2dfef5bdaed2d632ad5ff456689
Alexander Pinčuk <alexander.v.pinchuk@gmail.com>
2024-06-20 00:51:51 +0300

    Exclude applib snapshots from server synchronization
    Signed-off-by: Alexander Pinčuk <alexander.v.pinchuk@gmail.com>

--- nucleus/cluster/admin/src/main/java/com/sun/enterprise/v3/admin/cluster/ServerSynchronizer.java
+++ nucleus/cluster/admin/src/main/java/com/sun/enterprise/v3/admin/cluster/ServerSynchronizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ * Copyright (c) 2022, 2024 Contributors to the Eclipse Foundation.
  * Copyright (c) 2010, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -529,6 +529,7 @@ public final class ServerSynchronizer implements PostConstruct {
                                     throws URISyntaxException {
         List<String> skip = new ArrayList<>();
         skip.add("databases");
+        skip.add("snapshots");
         synchronizeDirectory(payload, server, sr,
                                 env.getLibPath(), skip, SyncLevel.RECURSIVE);
     }

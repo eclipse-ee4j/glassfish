f48de43b65ba477426c056a346564bcd5193d568
Ondro Mihalyi <mihalyi@omnifish.ee>
2024-09-12 02:30:32 +0200

    Runnable GF Embedded: Initial prototype
    Example of running an app from command line:
    
    java -jar glassfish-embedded-all.jar "set configs.config.server-config.network-config.network-listeners.network-listener.http-listener.enabled=true" "set configs.config.server-config.network-config.network-listeners.network-listener.http-listener.port=8080" app.war
    
    Example with root context:
    
    java -jar glassfish-embedded-all.jar "set configs.config.server-config.network-config.network-listeners.network-listener.http-listener.enabled=true" "set configs.config.server-config.network-config.network-listeners.network-listener.http-listener.port=8080" "deploy --contextroot=/ app.war"

---
+++ nucleus/core/bootstrap/src/main/java/com/sun/enterprise/glassfish/bootstrap/CommandLineParser.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package com.sun.enterprise.glassfish.bootstrap;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *
+ * @author Ondro Mihalyi
+ */
+public class CommandLineParser {
+    private String[] commandLineArgs;
+
+    public CommandLineParser(String[] commandLineArgs) {
+        this.commandLineArgs = commandLineArgs;
+    }
+
+    public static class Arguments {
+        public final List<String> commands = new ArrayList<>();
+        public final Properties properties = new Properties();
+    }
+
+    public Arguments parse() {
+        Arguments arguments = new Arguments();
+        for (String arg : commandLineArgs) {
+            if (isdeployable(arg)) {
+                arguments.commands.add("deploy " + arg);
+            } else {
+                arguments.commands.add(arg);
+            }
+        }
+        return arguments;
+    }
+
+    private boolean isdeployable(String arg) {
+        final File file = new File(arg);
+        return file.exists();
+    }
+
+}

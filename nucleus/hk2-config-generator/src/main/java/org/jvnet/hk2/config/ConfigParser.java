0d4fa5c9c99e5063c9704828c1bd69206d4c02d9
Ondro Mihalyi <mihalyi@omnifish.ee>
2024-09-17 11:36:34 +0200

    Runnable GF Embedded: Update copyright info

--- nucleus/hk2-config-generator/src/main/java/org/jvnet/hk2/config/ConfigParser.java
+++ nucleus/hk2-config-generator/src/main/java/org/jvnet/hk2/config/ConfigParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ * Copyright (c) 2023, 2024 Contributors to the Eclipse Foundation
  * Copyright (c) 2007, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the

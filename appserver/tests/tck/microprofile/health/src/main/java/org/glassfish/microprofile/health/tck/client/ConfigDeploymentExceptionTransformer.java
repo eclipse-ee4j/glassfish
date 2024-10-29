2d80b6d6b980953f2b8cd67136f6dfe5036ab96d
Thiago Henrique Hüpner <thihup@gmail.com>
2024-10-29 15:08:52 -0300

    Add license
    Signed-off-by: Thiago Henrique Hüpner <thihup@gmail.com>

--- appserver/tests/tck/microprofile/health/src/main/java/org/glassfish/microprofile/health/tck/client/ConfigDeploymentExceptionTransformer.java
+++ appserver/tests/tck/microprofile/health/src/main/java/org/glassfish/microprofile/health/tck/client/ConfigDeploymentExceptionTransformer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Contributors to Eclipse Foundation.
+ * Copyright (c) 2024 Contributors to Eclipse Foundation.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at

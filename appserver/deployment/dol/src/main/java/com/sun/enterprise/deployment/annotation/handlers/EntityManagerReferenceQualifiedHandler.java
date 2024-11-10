fdfef6c611241c859d46839457f1dc369aca857f
Arjan Tijms <arjan.tijms@gmail.com>
2024-11-10 17:39:46 +0100

    Fix copyright header

--- appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/annotation/handlers/EntityManagerReferenceQualifiedHandler.java
+++ appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/annotation/handlers/EntityManagerReferenceQualifiedHandler.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2022 Contributors to the Eclipse Foundation
- * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at

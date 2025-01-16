bc737dc1e2a6da850be066ba28563a8995b0af78
David Matějček <david.matejcek@omnifish.ee>
2025-01-16 21:47:34 +0100

    Fixed issue with spaces in native lib path
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- nucleus/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherNativeHelper.java
+++ nucleus/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherNativeHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Contributors to the Eclipse Foundation.
+ * Copyright (c) 2024, 2025 Contributors to the Eclipse Foundation.
  * Copyright (c) 2009, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -86,7 +86,7 @@ class GFLauncherNativeHelper {
         // * junk is removed, e.g. ":xxx::yy::::::" goes to "xxx:yy"
 
         String finalPathString = GFLauncherUtils.fileListToPathString(GFLauncherUtils.stringToFiles(sb.toString()));
-        String nativeCommand = "-D" + JAVA_NATIVE_SYSPROP_NAME + "=" + finalPathString;
+        String nativeCommand = "-D" + JAVA_NATIVE_SYSPROP_NAME + "=\"" + finalPathString + "\"";
         list.add(nativeCommand);
         return list;
     }

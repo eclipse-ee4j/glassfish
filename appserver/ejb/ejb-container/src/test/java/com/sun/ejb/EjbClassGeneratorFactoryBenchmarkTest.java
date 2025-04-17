dd284d11064bd01c9a42badd7fdacf0fa3fa9e8c
David Matějček <david.matejcek@omnifish.ee>
2025-04-17 14:01:29 +0200

    Increased limit for ratio of cache optimization test
    - On GitHub CI where the system has less CPU threads and lower performance it
      seems the caching doesn't help so much as on hardware with more power, where
      the ration is ie.
      - 67000/74 on my laptop
      - 370000/120000 on GitHub CI with Windows Server (but usually better)
    
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- appserver/ejb/ejb-container/src/test/java/com/sun/ejb/EjbClassGeneratorFactoryBenchmarkTest.java
+++ appserver/ejb/ejb-container/src/test/java/com/sun/ejb/EjbClassGeneratorFactoryBenchmarkTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Eclipse Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2025 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -92,7 +92,7 @@ public class EjbClassGeneratorFactoryBenchmarkTest {
         Collection<RunResult> results = new Runner(options).run();
         assertThat(results, hasSize(1));
         Result<?> primaryResult = results.iterator().next().getPrimaryResult();
-        assertThat(primaryResult.getScore(), lessThan(firstRunScore / 4));
+        assertThat(primaryResult.getScore(), lessThan(firstRunScore / 3));
     }
 
 

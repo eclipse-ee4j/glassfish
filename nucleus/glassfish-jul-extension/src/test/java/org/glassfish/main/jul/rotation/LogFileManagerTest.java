55a763e2ba96d1916db81bcba48326974ad02e8a
David Matějček <david.matejcek@omnifish.ee>
2025-05-02 22:17:00 +0200

    Replaced deprecated RandomStringUtils.randomAlphabetic
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- nucleus/glassfish-jul-extension/src/test/java/org/glassfish/main/jul/rotation/LogFileManagerTest.java
+++ nucleus/glassfish-jul-extension/src/test/java/org/glassfish/main/jul/rotation/LogFileManagerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2024 Eclipse Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2025 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -116,12 +116,12 @@ public class LogFileManagerTest {
         assertEquals(file.length(), manager.getFileSize());
 
         // limit is 100, so flush will roll.
-        manager.write(RandomStringUtils.randomAlphabetic(101));
+        manager.write(RandomStringUtils.insecure().next(101));
         manager.flush();
         assertEquals(0, manager.getFileSize());
 
         // disableOutput does not affect roll.
-        manager.write(RandomStringUtils.randomAlphabetic(101));
+        manager.write(RandomStringUtils.insecure().next(101));
         manager.disableOutput();
         manager.flush();
         assertEquals(0, manager.getFileSize());

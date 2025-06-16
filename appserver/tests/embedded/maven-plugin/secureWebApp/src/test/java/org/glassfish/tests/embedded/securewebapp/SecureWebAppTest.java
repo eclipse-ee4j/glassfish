73faf85cc08124a14f90f39afa2d37bc3ca6ee52
David Matějček <david.matejcek@omnifish.ee>
2025-06-16 16:47:33 +0200

    New KeyTool adapter
    - no dependencies on other GF sources
    - output is not read in parallel threads
    - we don't care about the language of the system, we just write password 4 times
    - then we read the output, basically we ignore it except logs.
    
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- appserver/tests/embedded/maven-plugin/secureWebApp/src/test/java/org/glassfish/tests/embedded/securewebapp/SecureWebAppTest.java
+++ appserver/tests/embedded/maven-plugin/secureWebApp/src/test/java/org/glassfish/tests/embedded/securewebapp/SecureWebAppTest.java
@@ -18,11 +18,13 @@
 package org.glassfish.tests.embedded.securewebapp;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
+import java.nio.file.Path;
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
@@ -33,6 +35,9 @@ import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.glassfish.main.jdke.security.KeyTool;
+import org.glassfish.tests.utils.junit.JUnitSystem;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -56,6 +61,13 @@ public class SecureWebAppTest {
             return;
         }
     }};
+
+    @BeforeAll
+    public static void createKeyStore() {
+        File keystore = JUnitSystem.detectBasedir().resolve(Path.of("target", "keystore.jks")).toFile();
+        KeyTool keyTool = new KeyTool(keystore, "changeit".toCharArray());
+        keyTool.generateKeyPair("s1as", "CN=localhost", "RSA", 1);
+    }
 
     @Test
     public void http() throws Exception {

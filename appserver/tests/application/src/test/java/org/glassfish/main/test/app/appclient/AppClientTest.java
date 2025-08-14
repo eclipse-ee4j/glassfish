4ea260f31c678ec87c6fcb069b8b7a38eff6e50a
David Matějček <david.matejcek@omnifish.ee>
2025-08-14 23:48:16 +0200

    Extended AppClientTest to verify also error outputs
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- appserver/tests/application/src/test/java/org/glassfish/main/test/app/appclient/AppClientTest.java
+++ appserver/tests/application/src/test/java/org/glassfish/main/test/app/appclient/AppClientTest.java
@@ -24,13 +24,15 @@ import java.nio.charset.Charset;
 import org.glassfish.main.itest.tools.GlassFishTestEnvironment;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.stringContainsInOrder;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- *
+ * @author David Matejcek
  */
 public class AppClientTest {
 
@@ -43,19 +45,38 @@ public class AppClientTest {
         processNoArgs.waitFor();
         processUsage.waitFor();
         String noArgsOutput = readOutput(processNoArgs);
+        String noArgsError = readError(processNoArgs);
         String usageOutput = readOutput(processUsage);
-        assertEquals(1, processNoArgs.exitValue(), "appclient without args exit code. Output: " + noArgsOutput);
-        assertEquals(0, processUsage.exitValue(), "appclient -usage exit code. Output: " + usageOutput);
-        assertThat("outputs should be same", usageOutput,
-            stringContainsInOrder("appclient [ <classfile> | -client <appjar> ]", "or",
-                "appclient [ <valid JVM options and valid ACC options> ]"));
-        assertEquals(usageOutput, noArgsOutput, "outputs should be same");
+        String usageError = readError(processUsage);
+        assertAll(
+            () -> assertEquals(1, processNoArgs.exitValue(),
+                "appclient without args exit code. Output:\n" + noArgsOutput + "\nError:\n" + noArgsError),
+            () -> assertEquals(0, processUsage.exitValue(),
+                "appclient -usage exit code. Output:\n" + usageOutput + "\nError:\n" + usageError)
+        );
+        assertThat("user output", usageOutput, stringContainsInOrder("appclient [ <classfile> | -client <appjar> ]",
+            "or", "appclient [ <valid JVM options and valid ACC options> ]"));
+        assertThat("error output", usageError, equalTo(""));
+        assertAll(
+            () -> assertEquals(usageOutput, noArgsOutput, "user outputs should be same"),
+            () -> assertEquals(usageError, noArgsError, "error outputs should be same")
+        );
     }
 
 
     private String readOutput(Process process) throws IOException {
         try (InputStream output = process.getInputStream()) {
-            return new String(output.readAllBytes(), Charset.defaultCharset());
+            return read(output);
         }
+    }
+
+    private String readError(Process process) throws IOException {
+        try (InputStream output = process.getErrorStream()) {
+            return read(output);
+        }
+    }
+
+    private String read(InputStream stream) throws IOException {
+        return new String(stream.readAllBytes(), Charset.defaultCharset());
     }
 }

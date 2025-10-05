0ff756f8f652019890c94f52aa1611fe8ac345ad
Ondro Mihalyi <mihalyi@omnifish.ee>
2025-10-05 22:08:22 +0200

    Embedded: Tests for setting sys props

--- appserver/tests/embedded/runnable/src/test/java/org/glassfish/tests/embedded/runnable/GfEmbeddedUtils.java
+++ appserver/tests/embedded/runnable/src/test/java/org/glassfish/tests/embedded/runnable/GfEmbeddedUtils.java
@@ -20,11 +20,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static java.lang.System.err;
+import static java.util.stream.Collectors.joining;
 
 /**
  *
@@ -32,15 +35,29 @@ import static java.lang.System.err;
  */
 public class GfEmbeddedUtils {
 
-    public static Process runGlassFishEmbedded(String glassfishEmbeddedJarName, String... additionalArguments) throws IOException {
+    public static final String DEBUG_PROPERTY_NAME = "glassfish.test.debug.port";
+
+    public static Process runGlassFishEmbedded(String glassfishEmbeddedJarName, String... additionalArguments) throws
+IOException {
+        return runGlassFishEmbedded(glassfishEmbeddedJarName, List.of(), additionalArguments);
+    }
+
+    public static Process runGlassFishEmbedded(String glassfishEmbeddedJarName, List<String> jvmOpts, String... additionalArguments) throws IOException {
         List<String> arguments = new ArrayList<>();
-        arguments.addAll(List.of(ProcessHandle.current().info().command().get(),
-                //                "-Xrunjdwp:transport=dt_socket,server=y,suspend=y", // enable debugging on random port
+        arguments.add(ProcessHandle.current().info().command().get());
+        addDebugArgsIfDebugEnabled(arguments);
+        arguments.addAll(jvmOpts);
+        arguments.addAll(List.of(
                 "-jar", glassfishEmbeddedJarName,
+                "--noPort",
                 "--stop"));
         for (String argument : additionalArguments) {
             arguments.add(argument);
         }
+        System.out.println("\nCurrent directory: " + Paths.get(".").toFile().getAbsolutePath());
+        System.out.println("Going to run Embedded GlassFish: " + arguments.stream()
+                .map(arg -> "'" + arg + "'")
+                .collect(joining(" ")) + "\n");
         return new ProcessBuilder()
                 .redirectOutput(ProcessBuilder.Redirect.INHERIT)
                 .redirectError(ProcessBuilder.Redirect.PIPE)
@@ -55,6 +72,17 @@ public class GfEmbeddedUtils {
         )
                 .lines()
                 .peek(err::println);
+    }
+
+    private static void addDebugArgsIfDebugEnabled(List<String> arguments) {
+        Optional.ofNullable(System.getProperty(DEBUG_PROPERTY_NAME))
+                .filter(debugPort -> List.of("none", "disabled", "false").stream()
+                        .allMatch(value -> !value.equalsIgnoreCase(debugPort))
+                )
+                .ifPresent(debugPort -> {
+                    arguments.add("-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:" + debugPort);
+                });
+
     }
 
 }

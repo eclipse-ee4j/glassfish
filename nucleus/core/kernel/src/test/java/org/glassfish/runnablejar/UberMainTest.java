9e48bb0130bff1be95887a42c12c3ff250ec1dba
David Matějček <david.matejcek@omnifish.ee>
2025-10-31 23:22:43 +0100

    Code cleanup, fixed test messages
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- nucleus/core/kernel/src/test/java/org/glassfish/runnablejar/UberMainTest.java
+++ nucleus/core/kernel/src/test/java/org/glassfish/runnablejar/UberMainTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ * Copyright (c) 2024, 2025 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -36,7 +36,9 @@ import org.glassfish.grizzly.config.dom.Ssl;
 import org.junit.jupiter.api.Test;
 import org.jvnet.hk2.config.types.Property;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.hasLength;
 
 /**
  *
@@ -50,7 +52,7 @@ public class UberMainTest {
             List.of(listener(8080, false), listener(8181, true)));
         System.out.println(info);
         String[] lines = info.split("\n");
-        assertTrue(lines.length == 6, "Number of lines should be 6 but is " + lines.length);
+        assertThat("Number of lines", lines, arrayWithSize(6));
         assertThatLinesAreEquallyLong(lines);
     }
 
@@ -60,36 +62,35 @@ public class UberMainTest {
             List.of());
         System.out.println(info);
         String[] lines = info.split("\n");
-        assertTrue(lines.length == 6, "Number of lines should be 6 but is " + lines.length);
+        assertThat("Number of lines", lines, arrayWithSize(6));
         assertThatLinesAreEquallyLong(lines);
     }
 
 
     @Test
-    public void getInfoAfterStartup_noApps() throws GlassFishException {
+    public void getInfoAfterStartup_noApps() {
         String info = new InfoPrinter().getInfoAfterStartup(List.of(),
             List.of(listener(8080, false), listener(8181, true)));
         System.out.println(info);
         String[] lines = info.split("\n");
-        assertTrue(lines.length == 5, "Number of lines should be 6 but is " + lines.length);
+        assertThat("Number of lines", lines, arrayWithSize(5));
         assertThatLinesAreEquallyLong(lines);
     }
 
     @Test
-    public void getInfoAfterStartup_noApps_noListeners() throws GlassFishException {
-        String info = new InfoPrinter().getInfoAfterStartup(List.of(),
-            List.of());
+    public void getInfoAfterStartup_noApps_noListeners() {
+        String info = new InfoPrinter().getInfoAfterStartup(List.of(), List.of());
         System.out.println(info);
         String[] lines = info.split("\n");
-        assertTrue(lines.length == 5, "Number of lines should be 6 but is " + lines.length);
+        assertThat("Number of lines", lines, arrayWithSize(5));
         assertThatLinesAreEquallyLong(lines);
     }
 
     private void assertThatLinesAreEquallyLong(String[] lines) {
         int lineLength = lines[0].length();
         for (int i = 1; i < lines.length; i++) {
-            int thisLineLength = lines[i].length();
-            assertTrue(thisLineLength == lineLength, "All lines should as long as the first line. The first line is " + lineLength + " long while the line number " + i + " is " + thisLineLength + " long");
+            assertThat("All lines should as long as the first line. Line with number " + i + " broke the rule.",
+                lines[i], hasLength(lineLength));
         }
     }
 
@@ -105,7 +106,7 @@ public class UberMainTest {
 
         private final String name;
 
-        public MockApplication(String name) {
+        MockApplication(String name) {
             this.name = name;
         }
 
@@ -285,7 +286,7 @@ public class UberMainTest {
         int port;
         boolean secure;
 
-        public MockListener(int port, boolean secure) {
+        MockListener(int port, boolean secure) {
             this.port = port;
             this.secure = secure;
         }

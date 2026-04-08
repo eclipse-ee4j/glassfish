b778b2c078616b306b7aebd3cf4bcfa926c33350
David Matějček <david.matejcek@omnifish.ee>
2026-04-09 01:44:19 +0200

    Separated test for windows and linux specific IPv6 addresses
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- nucleus/common/common-util/src/test/java/com/sun/enterprise/util/net/NetUtilsTest.java
+++ nucleus/common/common-util/src/test/java/com/sun/enterprise/util/net/NetUtilsTest.java
@@ -19,6 +19,9 @@ package com.sun.enterprise.util.net;
 import java.net.InetAddress;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -57,12 +60,23 @@ public class NetUtilsTest {
             () -> assertTrue(NetUtils.isLocal("localhost"), "hostname - localhost"),
             () -> assertTrue(NetUtils.isLocal("127.0.0.1"), "IPv4 address, 127.0.0.1"),
             () -> assertTrue(NetUtils.isLocal("0:0:0:0:0:0:0:1"), "IPv6 address 0:0:0:0:0:0:0:1, long format"),
-            () -> assertTrue(NetUtils.isLocal("::1%lo"), "IPv6 address ::1%lo, short format"),
             () -> assertTrue(NetUtils.isLocal(loopbackAddress.getHostAddress()), loopbackAddress.toString()),
             () -> assertTrue(NetUtils.isLocal(loopbackAddress.getHostName()), loopbackAddress.toString()),
             () -> assertTrue(NetUtils.isLocal(localHost.getHostAddress()), localHost.toString()),
             () -> assertTrue(NetUtils.isLocal(localHost.getHostName()), localHost.toString())
         );
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void isLocalHost_windows() {
+        assertTrue(NetUtils.isLocal("::1%1"), "IPv6 address ::1%1, short format");
+    }
+
+    @Test
+    @DisabledOnOs(OS.WINDOWS)
+    void isLocalHost_linux() {
+        assertTrue(NetUtils.isLocal("::1%lo"), "IPv6 address ::1%lo, short format");
     }
 
     @Test
